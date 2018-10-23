524e6b820e5e4a423b77700858ab65427e0885db
reacheight <reacheight8@gmail.com>
2018-10-23 19:57:18 +0300

    added course models

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HwProj.CoursesService.API.Models
+{
+    public class Course
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/CourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HwProj.CoursesService.API.Models
+{
+    public class CourseViewModel
+    {
+        public string Name { get; set; }
+    }
+}
