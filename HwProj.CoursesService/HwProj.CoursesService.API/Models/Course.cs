46212534fd96cc404f59ffb9ef3cc52309b27cfa
reacheight <reacheight8@gmail.com>
2018-10-27 18:24:58 +0300

    minor change

--- HwProj.CoursesService/HwProj.CoursesService.API/Models/Course.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace HwProj.CoursesService.API.Models
 {
     public class Course
     {
+        [Key]
         public long Id { get; set; }
         public string Name { get; set; }
     }
