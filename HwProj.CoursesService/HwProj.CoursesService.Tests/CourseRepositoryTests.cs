ff6b6caddcc71d2e8b21c295b4fb80aefeb4f151
reacheight <reacheight8@gmail.com>
2018-11-15 17:11:35 +0300

    added some course repository tests

--- HwProj.CoursesService/HwProj.CoursesService.Tests/CourseRepositoryTests.cs
+++ HwProj.CoursesService/HwProj.CoursesService.Tests/CourseRepositoryTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Shouldly;
 using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -13,6 +14,10 @@ namespace Tests
     {
         private SqliteConnection _connection;
         private DbContextOptions<CourseContext> _options;
+
+        private Course course1;
+        private Course course2;
+        private List<Course> courses;
 
         [SetUp]
         public void Setup()
@@ -28,6 +33,12 @@ namespace Tests
             {
                 context.Database.EnsureCreated();
             }
+
+            course1 = new Course() { Id = 1, Name = "course_name1", GroupName = "144", IsOpen = true };
+            course2 = new Course() { Id = 2, Name = "course_name2", GroupName = "243" };
+            courses = Enumerable.Range(1, 10)
+                .Select(i => new Course() { Id = i, Name = $"course{i}" })
+                .ToList();
         }
 
         [TearDown]
@@ -43,32 +54,22 @@ namespace Tests
         [Test]
         public async Task AddWritesToDatabase()
         {
-            var course = new Course()
-            {
-                Id = 1,
-                Name = "course_name"
-            };
-
             using (var context = new CourseContext(_options))
             {
                 var repository = new CourseRepository(context);
-                await repository.AddAsync(course);
+                await repository.AddAsync(course1);
             }
 
             using (var context = new CourseContext(_options))
             {
                 var addedCourse = await context.Courses.SingleAsync();
-                addedCourse.ShouldBe(course);
+                addedCourse.ShouldBe(course1);
             }
         }
 
         [Test]
         public void AddWritesSeveralCourses()
         {
-            var courses = Enumerable.Range(1, 10)
-                .Select(i => new Course() { Id = i, Name = $"course{i}" })
-                .ToList();
-
             using (var context = new CourseContext(_options))
             {
                 var repository = new CourseRepository(context);
@@ -86,35 +87,40 @@ namespace Tests
         [Test]
         public async Task GetAsyncGetsRightCourse()
         {
-            var course = new Course()
-            {
-                Id = 1,
-                Name = "course_name",
-                GroupName = "123"
-            };
-
             using (var context = new CourseContext(_options))
             {
                 var repository = new CourseRepository(context);
-                await repository.AddAsync(course);
+                await repository.AddAsync(course1);
             }
 
             using (var context = new CourseContext(_options))
             {
                 var repository = new CourseRepository(context);
-                var gottenCourse = await repository.GetAsync(course.Id);
+                var gottenCourse = await repository.GetAsync(course1.Id);
 
-                gottenCourse.ShouldBe(course);
+                gottenCourse.ShouldBe(course1);
+            }
+        }
+
+        [Test]
+        public async Task GetAsyncGetsNullOnInvalidId()
+        {
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                await repository.AddAsync(course1);
+            }
+
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                repository.GetAsync(5).Result.ShouldBeNull();
             }
         }
 
         [Test]
         public void CoursesGetsAllCourses()
         {
-            var courses = Enumerable.Range(1, 10)
-                .Select(i => new Course() { Id = i, Name = $"course{i}" })
-                .ToList();
-
             using (var context = new CourseContext(_options))
             {
                 var repository = new CourseRepository(context);
@@ -133,24 +139,159 @@ namespace Tests
         [Test]
         public async Task DeleteByIdDeletesFromDatabase()
         {
-            var course = new Course()
+            using (var context = new CourseContext(_options))
             {
-                Id = 1,
-                Name = "course_name"
-            };
+                var repository = new CourseRepository(context);
+                await repository.AddAsync(course1);
+            }
 
+            var deleted = false;
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                deleted = await repository.DeleteByIdAsync(course1.Id);
+            }
+
+            using (var context = new CourseContext(_options))
+            {
+                context.Courses.Count().ShouldBe(0);
+                deleted.ShouldBeTrue();
+            }
+        }
+
+        [Test]
+        public async Task DeleteByIdDeletesRightCourse()
+        {
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                await repository.AddAsync(course1);
+                await repository.AddAsync(course2);
+            }
+
+            var deleted = false;
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                deleted = await repository.DeleteByIdAsync(course2.Id);
+            }
+
+            using (var context = new CourseContext(_options))
+            {
+                context.Courses.Count().ShouldBe(1);
+                context.Courses.ShouldContain(course1);
+                deleted.ShouldBeTrue();
+            }
+        }
+
+        [Test]
+        public async Task DeleteByIdDontChangeDatabaseOnInvalidId()
+        {
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                await repository.AddAsync(course1);
+                await repository.AddAsync(course2);
+            }
+
+            var deleted = true;
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                deleted = await repository.DeleteByIdAsync(1001);
+            }
+
+            using (var context = new CourseContext(_options))
+            {
+                context.Courses.Count().ShouldBe(2);
+                context.Courses.ShouldContain(course1);
+                context.Courses.ShouldContain(course2);
+                deleted.ShouldBeFalse();
+            }
+        }
+
+        [Test]
+        public async Task UpdateWritesToDatabase()
+        {
+            var course = new Course() { Id = 100, Name = "java", GroupName = "144" };
             using (var context = new CourseContext(_options))
             {
                 var repository = new CourseRepository(context);
                 await repository.AddAsync(course);
             }
 
+            var courseViewModel = new CourseViewModel() { Name = "c#", GroupName = "244", IsOpen = true };
+            var updated = false;
             using (var context = new CourseContext(_options))
             {
                 var repository = new CourseRepository(context);
-                await repository.DeleteByIdAsync(course.Id);
+                updated = await repository.UpdateAsync(course.Id, courseViewModel);
+            }
 
-                context.Courses.Count().ShouldBe(0);
+            using (var context = new CourseContext(_options))
+            {
+                course.Name = courseViewModel.Name;
+                course.GroupName = courseViewModel.GroupName;
+                course.IsOpen = courseViewModel.IsOpen;
+
+                var updatedCourse = context.Courses.Find(course.Id);
+                updatedCourse.ShouldBe(course);
+                updated.ShouldBeTrue();
+            }
+        }
+
+        [Test]
+        public async Task UpdateModifiesRightCourse()
+        {
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                await repository.AddAsync(course1);
+                await repository.AddAsync(course2);
+            }
+
+            var courseViewModel = new CourseViewModel() { Name = "java", GroupName = "144", IsOpen = true };
+            var updated = false;
+            using (var context = new CourseContext(_options))
+            {
+                var reposository = new CourseRepository(context);
+                updated = await reposository.UpdateAsync(course1.Id, courseViewModel);
+            }
+
+            using (var context = new CourseContext(_options))
+            {
+                course1.Name = courseViewModel.Name;
+                var updatedCourse = context.Courses.Find(course1.Id);
+
+                updatedCourse.ShouldBe(course1);
+                context.Courses.ShouldContain(course2);
+                context.Courses.Count().ShouldBe(2);
+                updated.ShouldBeTrue();
+            }
+        }
+
+        [Test]
+        public async Task UpdateDontChangeDatabaseOnInvalidId()
+        {
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                await repository.AddAsync(course1);
+            }
+
+            var courseViewModel = new CourseViewModel() { Name = "c#", GroupName = "244", IsOpen = true };
+            var updated = true;
+            using (var context = new CourseContext(_options))
+            {
+                var repository = new CourseRepository(context);
+                updated = await repository.UpdateAsync(800, courseViewModel);
+            }
+
+            using (var context = new CourseContext(_options))
+            {
+                context.Courses.ShouldContain(course1);
+                context.Courses.Count().ShouldBe(1);
+                updated.ShouldBeFalse();
             }
         }
     }
