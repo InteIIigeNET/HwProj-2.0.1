3d69838f5f4f574cca72e1eb68eca9df7cdd6d92
reacheight <reacheight8@gmail.com>
2018-12-09 10:49:55 +0300

    file renamed

--- HwProj.CoursesService/HwProj.CoursesService.API/ApplicationProfile.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/ApplicationProfile.cs
@@ -1,10 +1,6 @@
 ﻿using AutoMapper;
 using HwProj.CoursesService.API.Models;
 using HwProj.CoursesService.API.Models.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HwProj.CoursesService.API
 {
