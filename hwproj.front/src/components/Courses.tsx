72be93ab2f741359612b4734386352e51065db8d
reacheight <reacheight8@gmail.com>
2018-12-13 12:33:30 +0300

    added courses component

---
+++ hwproj.front/src/components/Courses.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import CoursesApi from '../api/CoursesApi'
+import ICourse from '../models/Course'
+
+export interface IState {
+    isLoaded: boolean;
+    courses: ICourse[];
+}
+
+export default class Courses extends React.Component<{}, IState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = { isLoaded: false, courses: [] };
+    }
+
+    public render() {
+        if (this.state.isLoaded) {
+            const listItems = this.state.courses.map(course => (<li>{course.name}</li>));
+            
+            return (
+                <ul>
+                    {listItems}
+                </ul>
+            );
+        }
+        else {
+            return (<div/>);
+        }
+    }
+
+    public componentDidMount() {
+        CoursesApi.getAllCourses().then(data => {
+            this.setState({ isLoaded: true, courses: data })
+        });
+      }
+}
