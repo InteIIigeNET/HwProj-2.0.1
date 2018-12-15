08c7dfece2517997fb623bd276692c86a90a6178
reacheight <reacheight8@gmail.com>
2018-12-15 17:21:10 +0300

    implemented base read only repository

--- HwProj.CoursesService/HwProj.CoursesService.API/Models/Repositories/BaseRepository.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Repositories/BaseRepository.cs
@@ -8,38 +8,22 @@ using Z.EntityFramework.Plus;
 
 namespace HwProj.CoursesService.API.Models.Repositories
 {
-    public abstract class BaseRepository<T> : ICrudRepository<T>
+    public abstract class BaseRepository<T> : ReadOnlyRepository<T>, ICrudRepository<T>
         where T : class, new()
     {
         private readonly DbContext _context;
 
         public BaseRepository(DbContext context)
+            : base(context)
         {
             _context = context;
         }
-
-        protected abstract IQueryable<T> GetEntities();
-
-        protected IQueryable<T> GetAllEntites()
-            => _context.Set<T>();
-
-        public IReadOnlyCollection<T> GetAll()
-            => GetEntities()
-                .AsNoTracking().ToArray();
-
-        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate)
-            => await GetEntities()
-                .Where(predicate)
-                .FirstOrDefaultAsync();
         
         public async Task AddAsync(T item)
         {
             await _context.Set<T>().AddAsync(item);
             await _context.SaveChangesAsync();
         }
-
-        public async Task<bool> ContainsAsync(Expression<Func<T, bool>> predicate)
-            => await _context.Set<T>().Where(predicate).AnyAsync();
 
         public async Task<bool> DeleteAsync(Expression<Func<T, bool>> predicate)
             => await _context.Set<T>().Where(predicate).DeleteAsync() == 1;
@@ -49,6 +33,5 @@ namespace HwProj.CoursesService.API.Models.Repositories
 
         protected Task SaveAsync()
             => _context.SaveChangesAsync();
-
     }
 }
---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Repositories/ReadOnlyRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HwProj.CoursesService.API.Models.Repositories
+{
+    public abstract class ReadOnlyRepository<T> : IReadOnlyRepository<T>
+        where T : class, new()
+    {
+        private readonly DbContext _context;
+
+        public ReadOnlyRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        protected abstract IQueryable<T> GetEntities();
+
+        protected IQueryable<T> GetAllEntites()
+            => _context.Set<T>();
+
+        public IReadOnlyCollection<T> GetAll()
+            => GetEntities()
+                    .AsNoTracking().ToArray();
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate)
+            => await GetEntities()
+                .Where(predicate)
+                .FirstOrDefaultAsync();
+
+        public async Task<bool> ContainsAsync(Expression<Func<T, bool>> predicate)
+            => await _context.Set<T>().Where(predicate).AnyAsync();
+    }
+}
