fadf1c8af64e457799846c1ab816e674493fb34b
reacheight <reacheight8@gmail.com>
2018-12-17 13:51:31 +0300

    added some task api methods

--- HwProj.TasksService/HwProj.TasksService.API/Controllers/TasksController.cs
+++ HwProj.TasksService/HwProj.TasksService.API/Controllers/TasksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using HwProj.TasksService.API.Models;
 using HwProj.TasksService.API.Models.Repositories;
 using HwProj.TasksService.API.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
@@ -25,10 +26,19 @@ namespace HwProj.TasksService.API.Controllers
             _mapper = mapper;
         }
 
+        [HttpGet("{taskId}")]
+        public async Task<IActionResult> GetTask(long taskId)
+        {
+            var task = await _taskRepository.GetAsync(t => t.Id == taskId);
+            return task == null
+                ? NotFound() as IActionResult
+                : Ok(_mapper.Map<TaskViewModel>(task));
+        }
+
         [HttpPost("add_task/{homeworkId}")]
         public async Task<IActionResult> AddTask(long homeworkId, [FromBody]CreateTaskViewModel taskViewModel)
         {
-            var task = _mapper.Map<Models.HomeworkTask>(taskViewModel);
+            var task = _mapper.Map<HomeworkTask>(taskViewModel);
             await _homeworkRepository.AddTask(homeworkId, task);
 
             return Ok(_mapper.Map<TaskViewModel>(task));
@@ -41,6 +51,29 @@ namespace HwProj.TasksService.API.Controllers
             return homework == null
                 ? NotFound() as IActionResult
                 : Json(homework.Tasks.Select(t => _mapper.Map<TaskViewModel>(t)));
+        }
+
+        [HttpPost("update_task/{taskId}")]
+        public async Task<IActionResult> UpdateTask(long taskId, [FromBody]CreateTaskViewModel taskViewModel)
+        {
+            var updated = await _taskRepository.UpdateAsync(t => t.Id == taskId, t => new HomeworkTask()
+            {
+                Title = taskViewModel.Title,
+                Description = taskViewModel.Description
+            });
+
+            return updated
+                ? Ok()
+                : NotFound() as IActionResult;
+        }
+
+        [HttpDelete("delete_task/{taskId}")]
+        public async Task<IActionResult> DeleteTask(long taskId)
+        {
+            var deleted = await _taskRepository.DeleteAsync(t => t.Id == taskId);
+            return deleted
+                ? Ok()
+                : NotFound() as IActionResult;
         }
     }
 }
