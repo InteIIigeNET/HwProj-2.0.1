50835f17e28041654feada4bd846636e8c746234
reacheight <reacheight8@gmail.com>
2018-12-17 12:51:22 +0300

    added init project and some models

---
+++ HwProj.TasksService/HwProj.TasksService.API/Models/Homework.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HwProj.TasksService.API.Models
+{
+    public class Homework
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public long Id { get; set; }
+
+        public List<Task> Tasks { get; set; } = new List<Task>();
+    }
+}
---
+++ HwProj.TasksService/HwProj.TasksService.API/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HwProj.TasksService.API.Models
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public long HomeworkId { get; set; }
+        public Homework Homework { get; set; }
+    }
+}
