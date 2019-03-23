06763c235faa59f5614dc4e83a9bde94f69d57ca
reacheight <reacheight8@gmail.com>
2019-03-23 12:37:06 +0300

    added repository interfaces

---
+++ HwProj.Repositories/ICrudRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HwProj.Repositories
+{
+    public interface ICrudRepository<TEntity> : IReadOnlyRepository<TEntity>
+        where TEntity : IEntity
+    {
+        void Add(TEntity item);
+        bool Delete(long id);
+        bool Update(long id, Expression<Func<TEntity, TEntity>> updateFactory);
+        Task AddAsync(TEntity item);
+        Task<bool> DeleteAsync(long id);
+        Task<bool> UpdateAsync(long id, Expression<Func<TEntity, TEntity>> updateFactory);
+    }
+}
