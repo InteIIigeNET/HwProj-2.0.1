2194a39a2c21200397dddbb7e522ff9aae0a400d
reacheight <reacheight8@gmail.com>
2019-03-23 12:37:24 +0300

    implemented read only and crud repositories

---
+++ HwProj.Repositories/CrudRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Z.EntityFramework.Plus;
+
+namespace HwProj.Repositories
+{
+    public class CrudRepository<TEntity> : ReadOnlyRepository<TEntity>, ICrudRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        public CrudRepository(DbContext context)
+            : base(context)
+        {
+        }
+        
+        public void Add(TEntity item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+        }
+
+        public bool Delete(long id)
+            => _context.Set<TEntity>().Where(entity => entity.Id == id).Delete() == 1;
+
+        public bool Update(long id, Expression<Func<TEntity, TEntity>> updateFactory)
+            => _context.Set<TEntity>().Where(entity => entity.Id == id).Update(updateFactory) == 1;
+
+        public async Task AddAsync(TEntity item)
+        {
+            await _context.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(long id)
+            => await _context.Set<TEntity>().Where(entity => entity.Id == id).DeleteAsync() == 1;
+
+        public async Task<bool> UpdateAsync(long id, Expression<Func<TEntity, TEntity>> updateFactory)
+            => await _context.Set<TEntity>().Where(entity => entity.Id == id).UpdateAsync(updateFactory) == 1;
+    }
+}
---
+++ HwProj.Repositories/ReadOnlyRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HwProj.Repositories
+{
+    public class ReadOnlyRepository<TEntity> : IReadOnlyRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        protected readonly DbContext _context;
+
+        public ReadOnlyRepository(DbContext context)
+        {
+            _context = context;
+        }
+        
+        public TEntity Get(long id)
+            => _context.Find<TEntity>(id);
+
+        public IReadOnlyCollection<TEntity> GetAll()
+            => _context.Set<TEntity>().AsNoTracking().ToArray();
+
+        public TEntity Find(Func<TEntity, bool> predicate)
+            => _context.Set<TEntity>().FirstOrDefault(predicate);
+
+        public IReadOnlyCollection<TEntity> FindAll(Func<TEntity, bool> predicate)
+            => _context.Set<TEntity>().AsNoTracking().AsEnumerable().Where(predicate).ToArray();
+
+        public Task<TEntity> GetAsync(long id)
+            => _context.FindAsync<TEntity>(id);
+
+        public Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> predicate)
+            => _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+    }
+}
