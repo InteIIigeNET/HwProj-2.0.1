f9ae6fffcd831bdc22e0b3a4608438e4ff51ed09
reacheight <reacheight8@gmail.com>
2019-03-23 12:37:06 +0300

    added repository interfaces

---
+++ HwProj.Repositories/IReadOnlyRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HwProj.Repositories
+{
+    public interface IReadOnlyRepository<TEntity>
+        where TEntity : IEntity
+    {
+        TEntity Get(long id);
+        IReadOnlyCollection<TEntity> GetAll();
+        TEntity Find(Func<TEntity, bool> predicate);
+        IReadOnlyCollection<TEntity> FindAll(Func<TEntity, bool> predicate);
+        Task<TEntity> GetAsync(long id);
+        Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
