bf465dd39fec26dd4105ef6899b70bd7209df553
reacheight <reacheight8@gmail.com>
2019-03-27 16:03:21 +0300

    added empty solutions controller

---
+++ HwProj.SolutionsService/HwProj.SolutionsService.API/Controllers/SolutionsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HwProj.SolutionsService.API.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HwProj.SolutionsService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolutionsController : Controller
+    {
+        private readonly ISolutionRepository _solutionRepository;
+
+        public SolutionsController(ISolutionRepository solutionRepository)
+        {
+            _solutionRepository = solutionRepository;
+        }
+    }
+}
