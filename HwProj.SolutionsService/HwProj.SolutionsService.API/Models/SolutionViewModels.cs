deaccaae412df49e8786f9d8f4e62c4760f5b7ce
reacheight <reacheight8@gmail.com>
2019-03-27 16:08:24 +0300

    added solution view model

---
+++ HwProj.SolutionsService/HwProj.SolutionsService.API/Models/SolutionViewModels.cs
@@ -0,0 +1,9 @@
+﻿namespace HwProj.SolutionsService.API.Models
+{
+    public class CreateSolutionViewModel
+    {
+        public string GithubUrl { get; set; }
+        
+        public string Comment { get; set; }
+    }
+}
