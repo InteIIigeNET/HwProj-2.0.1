27d7517701eed8d18e07229a0a9f77c6049c6b70
Girin Alexey <girin.alexey@gmail.com>
2019-04-04 01:31:07 +0300

    написал APIGateway, который должен перенаправлять запросы в тестовый микросервис TestAPI

---
+++ HwProj.APIGateway/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HwProj.APIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHostBuilder builder = new WebHostBuilder();
+            builder.ConfigureServices(s =>
+            {
+                s.AddSingleton(builder);
+            });
+            builder.UseKestrel()
+                   .UseContentRoot(Directory.GetCurrentDirectory())
+                   .UseStartup<Startup>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
---
+++ HwProj.TestAPI/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HwProj.TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "success";
+        }
+    }
+}
