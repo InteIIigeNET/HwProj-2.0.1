7233d6374849339a6cba364b9c9fdc833b5c0c83
reacheight <reacheight8@gmail.com>
2019-04-04 15:06:48 +0300

    updated repositories

--- HwProj.Repositories/IReadOnlyRepository.cs
+++ HwProj.Repositories/IReadOnlyRepository.cs
@@ -13,6 +13,17 @@ namespace HwProj.Repositories
         TEntity Find(Func<TEntity, bool> predicate);
         IReadOnlyCollection<TEntity> FindAll(Expression<Func<TEntity, bool>> predicate);
         Task<TEntity> GetAsync(long id);
+        Task<TEntity[]> GetAllAsync();
         Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity[]> FindAllAsync(Expression<Func<TEntity, bool>> predicate);
+        
+        TEntity Get<TProperty>(long id, Expression<Func<TEntity,TProperty>> includeProperty);
+        IReadOnlyCollection<TEntity> GetAll<TProperty>(Expression<Func<TEntity,TProperty>> includeProperty);
+        TEntity Find<TProperty>(Func<TEntity, bool> predicate, Expression<Func<TEntity,TProperty>> includeProperty);
+        IReadOnlyCollection<TEntity> FindAll<TProperty>(Expression<Func<TEntity, bool>> predicate, Expression<Func<TEntity,TProperty>> includeProperty);
+        Task<TEntity> GetAsync<TProperty>(long id, Expression<Func<TEntity,TProperty>> includeProperty);
+        Task<TEntity[]> GetAllAsync<TProperty>(Expression<Func<TEntity, TProperty>> includeProperty);
+        Task<TEntity> FindAsync<TProperty>(Expression<Func<TEntity, bool>> predicate, Expression<Func<TEntity,TProperty>> includeProperty);
+        Task<TEntity[]> FindAllAsync<TProperty>(Expression<Func<TEntity, bool>> predicate, Expression<Func<TEntity,TProperty>> includeProperty);
     }
 }
--- HwProj.Repositories/ReadOnlyRepository.cs
+++ HwProj.Repositories/ReadOnlyRepository.cs
@@ -32,7 +32,41 @@ namespace HwProj.Repositories
         public Task<TEntity> GetAsync(long id)
             => _context.FindAsync<TEntity>(id);
 
+        public Task<TEntity[]> GetAllAsync()
+            => _context.Set<TEntity>().ToArrayAsync();
+
         public Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> predicate)
             => _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+
+        public Task<TEntity[]> FindAllAsync(Expression<Func<TEntity, bool>> predicate)
+            => _context.Set<TEntity>().Where(predicate).ToArrayAsync();
+        
+        public TEntity Get<TProperty>(long id, Expression<Func<TEntity, TProperty>> includeQuery)
+            => _context.Set<TEntity>().Include(includeQuery).FirstOrDefault(e => e.Id == id);
+
+        public IReadOnlyCollection<TEntity> GetAll<TProperty>(Expression<Func<TEntity, TProperty>> includeQuery)
+            => _context.Set<TEntity>().Include(includeQuery).AsNoTracking().ToArray();
+
+        public TEntity Find<TProperty>(Func<TEntity, bool> predicate,
+            Expression<Func<TEntity, TProperty>> includeQuery)
+            => _context.Set<TEntity>().Include(includeQuery).FirstOrDefault(predicate);
+
+        public IReadOnlyCollection<TEntity> FindAll<TProperty>(Expression<Func<TEntity, bool>> predicate,
+            Expression<Func<TEntity, TProperty>> includeQuery)
+            => _context.Set<TEntity>().Include(includeQuery).Where(predicate).AsNoTracking().ToArray();
+
+        public Task<TEntity> GetAsync<TProperty>(long id, Expression<Func<TEntity, TProperty>> includeQuery)
+            => _context.Set<TEntity>().Include(includeQuery).FirstOrDefaultAsync(e => e.Id == id);
+        
+        public Task<TEntity[]> GetAllAsync<TProperty>(Expression<Func<TEntity, TProperty>> includeQuery)
+            => _context.Set<TEntity>().Include(includeQuery).AsNoTracking().ToArrayAsync();
+
+        public Task<TEntity> FindAsync<TProperty>(Expression<Func<TEntity, bool>> predicate,
+            Expression<Func<TEntity, TProperty>> includeQuery)
+            => _context.Set<TEntity>().Include(includeQuery).FirstOrDefaultAsync(predicate);
+
+        public Task<TEntity[]> FindAllAsync<TProperty>(Expression<Func<TEntity, bool>> predicate,
+            Expression<Func<TEntity, TProperty>> includeQuery)
+            => _context.Set<TEntity>().Include(includeQuery).Where(predicate).AsNoTracking().ToArrayAsync();
     }
 }
