cce5a1b7939493fc299eb5b1c83a7bba3586bd19
reacheight <reacheight8@gmail.com>
2019-05-15 18:29:48 +0300

    added back-to-course link to edit course page"

--- hwproj.front/src/components/EditCourse.tsx
+++ hwproj.front/src/components/EditCourse.tsx
@@ -6,7 +6,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Typography from '@material-ui/core/Typography'
 import { Redirect } from 'react-router-dom';
 import { CoursesApi } from "../api/courses/api";
-import {RouteComponentProps} from "react-router-dom"
+import {RouteComponentProps, Link} from "react-router-dom"
 
 interface IEditCourseState {
     name: string,
@@ -64,55 +64,60 @@ export default class EditCourse extends React.Component<RouteComponentProps<IEdi
             return <Redirect to='/' />
         }
         return (
-            <div className="container">
-                <Typography variant='h6' gutterBottom>Редактировать курс</Typography>
-                <form onSubmit={e => this.handleSubmit(e)}>
-                    <TextField
-                        required
-                        label="Название курса"
-                        variant="outlined"
-                        margin="normal"
-                        value={this.state.name}
-                        onChange={e => this.setState({ name: e.target.value })}
-                    />
+            <div>
+                &nbsp; <Link to={'/courses/' + this.props.match.params.courseId}>Назад к курсу</Link>
                     <br />
-                    <TextField
-                        required
-                        label="Номер группы"
-                        variant="outlined"
-                        margin="normal"
-                        value={this.state.groupName}
-                        onChange={e => this.setState({ groupName: e.target.value})}
-                    />
                     <br />
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                            defaultChecked
-                            color="primary"
-                            checked={this.state.isOpen}
-                            onChange={e => this.setState({ isOpen: e.target.checked})}
+                <div className="container">
+                    <Typography variant='h6' gutterBottom>Редактировать курс</Typography>
+                    <form onSubmit={e => this.handleSubmit(e)}>
+                        <TextField
+                            required
+                            label="Название курса"
+                            variant="outlined"
+                            margin="normal"
+                            value={this.state.name}
+                            onChange={e => this.setState({ name: e.target.value })}
                         />
-                        }
-                        label="Открытый курс"
-                    />
-                     <FormControlLabel
-                        control={
-                            <Checkbox
-                            defaultChecked
-                            color="primary"
-                            checked={this.state.isComplete}
-                            onChange={e => this.setState({ isComplete: e.target.checked})}
+                        <br />
+                        <TextField
+                            required
+                            label="Номер группы"
+                            variant="outlined"
+                            margin="normal"
+                            value={this.state.groupName}
+                            onChange={e => this.setState({ groupName: e.target.value})}
                         />
-                        }
-                        label="Завершённый курс"
-                    />
-                    <br />
-                    <Button size="small" variant="contained" color="primary" type="submit">Редактировать курс</Button>
-                    <br />
-                    <br />
-                    <Button onClick={() => this.onDelete()} size="small" variant="contained" color="secondary">Удалить курс</Button>
-                </form>
+                        <br />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                defaultChecked
+                                color="primary"
+                                checked={this.state.isOpen}
+                                onChange={e => this.setState({ isOpen: e.target.checked})}
+                            />
+                            }
+                            label="Открытый курс"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                defaultChecked
+                                color="primary"
+                                checked={this.state.isComplete}
+                                onChange={e => this.setState({ isComplete: e.target.checked})}
+                            />
+                            }
+                            label="Завершённый курс"
+                        />
+                        <br />
+                        <Button size="small" variant="contained" color="primary" type="submit">Редактировать курс</Button>
+                        <br />
+                        <br />
+                        <Button onClick={() => this.onDelete()} size="small" variant="contained" color="secondary">Удалить курс</Button>
+                    </form>
+                </div>
             </div>
         );
     }
