adf1fc3c926e072ee6f5b3d74307083b5757aff4
reacheight <reacheight8@gmail.com>
2019-05-18 14:57:21 +0300

    now edit course page is shown only for mentor

--- hwproj.front/src/components/EditCourse.tsx
+++ hwproj.front/src/components/EditCourse.tsx
@@ -7,12 +7,15 @@ import Typography from '@material-ui/core/Typography'
 import { Redirect } from 'react-router-dom';
 import { CoursesApi } from "../api/courses/api";
 import {RouteComponentProps, Link} from "react-router-dom"
+import AuthService from './AuthService'
 
 interface IEditCourseState {
+    isLoaded: boolean,
     name: string,
     groupName: string,
     isOpen: boolean,
     isComplete: boolean,
+    mentorId: string,
     edited: boolean,
     deleted: boolean
 }
@@ -22,13 +25,17 @@ interface IEditCourseProps {
 }
 
 export default class EditCourse extends React.Component<RouteComponentProps<IEditCourseProps>, IEditCourseState> {
+    authService = new AuthService();
+    coursesClient = new CoursesApi();
     constructor(props: RouteComponentProps<IEditCourseProps>) {
         super(props)
         this.state = {
+            isLoaded: false,
             name: "",
             groupName: "",
             isOpen: false,
             isComplete: false,
+            mentorId: "",
             edited: false,
             deleted: false
         };
@@ -36,7 +43,6 @@ export default class EditCourse extends React.Component<RouteComponentProps<IEdi
             
     public handleSubmit(e: any) {
         e.preventDefault();
-        let api = new CoursesApi();
 
         let courseViewModel = {
             name: this.state.name,
@@ -45,7 +51,7 @@ export default class EditCourse extends React.Component<RouteComponentProps<IEdi
             isComplete: this.state.isComplete
         };
 
-        api.updateCourse(+this.props.match.params.courseId, courseViewModel)
+        this.coursesClient.updateCourse(+this.props.match.params.courseId, courseViewModel)
             .then(res => this.setState({edited: true}))
     }
 
@@ -56,81 +62,91 @@ export default class EditCourse extends React.Component<RouteComponentProps<IEdi
     }
 
     public render() {
-        if (this.state.edited) {
-            return <Redirect to={'/courses/' + this.props.match.params.courseId} />
+        if (this.state.isLoaded) {
+            if (this.state.edited) {
+                return <Redirect to={'/courses/' + this.props.match.params.courseId} />
+            }
+
+            if (this.state.deleted) {
+                return <Redirect to='/' />
+            }
+
+            if (!this.authService.loggedIn() || this.authService.getProfile()._id !== this.state.mentorId) {
+                return <Typography variant='h6' gutterBottom>Только преподаватель может редактировать курс</Typography>
+            }
+
+            return (
+                <div>
+                    &nbsp; <Link to={'/courses/' + this.props.match.params.courseId}>Назад к курсу</Link>
+                        <br />
+                        <br />
+                    <div className="container">
+                        <Typography variant='h6' gutterBottom>Редактировать курс</Typography>
+                        <form onSubmit={e => this.handleSubmit(e)}>
+                            <TextField
+                                required
+                                label="Название курса"
+                                variant="outlined"
+                                margin="normal"
+                                value={this.state.name}
+                                onChange={e => this.setState({ name: e.target.value })}
+                            />
+                            <br />
+                            <TextField
+                                required
+                                label="Номер группы"
+                                variant="outlined"
+                                margin="normal"
+                                value={this.state.groupName}
+                                onChange={e => this.setState({ groupName: e.target.value})}
+                            />
+                            <br />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                    defaultChecked
+                                    color="primary"
+                                    checked={this.state.isOpen}
+                                    onChange={e => this.setState({ isOpen: e.target.checked})}
+                                />
+                                }
+                                label="Открытый курс"
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                    defaultChecked
+                                    color="primary"
+                                    checked={this.state.isComplete}
+                                    onChange={e => this.setState({ isComplete: e.target.checked})}
+                                />
+                                }
+                                label="Завершённый курс"
+                            />
+                            <br />
+                            <Button size="small" variant="contained" color="primary" type="submit">Редактировать курс</Button>
+                            <br />
+                            <br />
+                            <Button onClick={() => this.onDelete()} size="small" variant="contained" color="secondary">Удалить курс</Button>
+                        </form>
+                    </div>
+                </div>
+            );
         }
 
-        if (this.state.deleted) {
-            return <Redirect to='/' />
-        }
-        return (
-            <div>
-                &nbsp; <Link to={'/courses/' + this.props.match.params.courseId}>Назад к курсу</Link>
-                    <br />
-                    <br />
-                <div className="container">
-                    <Typography variant='h6' gutterBottom>Редактировать курс</Typography>
-                    <form onSubmit={e => this.handleSubmit(e)}>
-                        <TextField
-                            required
-                            label="Название курса"
-                            variant="outlined"
-                            margin="normal"
-                            value={this.state.name}
-                            onChange={e => this.setState({ name: e.target.value })}
-                        />
-                        <br />
-                        <TextField
-                            required
-                            label="Номер группы"
-                            variant="outlined"
-                            margin="normal"
-                            value={this.state.groupName}
-                            onChange={e => this.setState({ groupName: e.target.value})}
-                        />
-                        <br />
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                defaultChecked
-                                color="primary"
-                                checked={this.state.isOpen}
-                                onChange={e => this.setState({ isOpen: e.target.checked})}
-                            />
-                            }
-                            label="Открытый курс"
-                        />
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                defaultChecked
-                                color="primary"
-                                checked={this.state.isComplete}
-                                onChange={e => this.setState({ isComplete: e.target.checked})}
-                            />
-                            }
-                            label="Завершённый курс"
-                        />
-                        <br />
-                        <Button size="small" variant="contained" color="primary" type="submit">Редактировать курс</Button>
-                        <br />
-                        <br />
-                        <Button onClick={() => this.onDelete()} size="small" variant="contained" color="secondary">Удалить курс</Button>
-                    </form>
-                </div>
-            </div>
-        );
+        return "";
     }
 
     componentDidMount() {
-        let api = new CoursesApi();
-        api.get(+this.props.match.params.courseId)
+        this.coursesClient.get(+this.props.match.params.courseId)
             .then(res => res.json())
             .then(course => this.setState({
+                isLoaded: true,
                 name: course.name,
                 groupName: course.groupName,
                 isOpen: course.isOpen,
-                isComplete: course.isComplete
+                isComplete: course.isComplete,
+                mentorId: course.mentorId
             }))
     }
 }
