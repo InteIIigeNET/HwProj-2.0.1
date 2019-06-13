7c549533137e2244c236a53179457d2d726077bc
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2019-06-13 23:39:19 +0300

    чиню билд

--- HwProj.NotificationsService/HwProj.NotificationsService.API/Startup.cs
+++ HwProj.NotificationsService/HwProj.NotificationsService.API/Startup.cs
@@ -4,7 +4,6 @@ using HwProj.NotificationsService.API.Services;
 using HwProj.Utils.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
