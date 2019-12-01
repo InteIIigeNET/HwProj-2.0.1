55195b37fa0d21c73eec6d1ee13201a839451d17
usik-vladislav <31969107+usik-vladislav@users.noreply.github.com>
2019-12-01 21:36:19 +0300

    Changed for Codacy v2

--- SecondTestUserService/Program.cs
+++ SecondTestUserService/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace SecondTestUserService
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
