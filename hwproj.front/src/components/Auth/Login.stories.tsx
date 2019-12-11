ac6482b915a9256d6b014c20cb697ca966efd966
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2019-12-11 20:09:07 +0300

    Create Login.stories.tsx

---
+++ hwproj.front/src/components/Auth/Login.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import Login from "./Login";
+
+storiesOf("Login page", module)
+  .add("simple", () =>
+    <Login/>
+    );
