ee429eabda2c6237085c690ac1efacdb57ac1763
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2019-12-11 17:16:23 +0300

    Fix problem with storybook

--- hwproj.front/src/components/Courses/CoursesList.stories.tsx
+++ hwproj.front/src/components/Courses/CoursesList.stories.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
+import { CourseViewModel } from "../../api/courses/api";
 import { CoursesList } from "./CoursesList";
 
-const data = [{id: 0, name: "Programming", groupName: "241", }];
+const data : CourseViewModel[] = [{id: 0, name: "Programming", groupName: "241", }];
 
 storiesOf("Courses list page", module)
   .add("simple", () =>
--- hwproj.front/src/components/Courses/CoursesList.tsx
+++ hwproj.front/src/components/Courses/CoursesList.tsx
@@ -1,27 +1,27 @@
 import * as React from "react";
 import { CourseViewModel } from "../../api/courses/api";
-import { Link as RouterLink} from "react-router-dom";
+import { Link as RouterLink, BrowserRouter as Router } from "react-router-dom";
 
 interface ICoursesProps {
     courses: CourseViewModel[];
 }
 
-export default class CoursesList extends React.Component< ICoursesProps > {
+export class CoursesList extends React.Component<ICoursesProps, {}> {
     public render() {
         const { courses } = this.props;
 
         return (
             <div className="container">
-                {
-                    courses.map(course =>
+                <Router>
+                {courses.map(course =>
                     <li key={course.id}>
                         <RouterLink to={"/courses/" + course.id!.toString()}>
                             {course.name}
                         </RouterLink>
                         <br />
                         {course.groupName}
-                    </li>).reverse()
-                }
+                    </li>)}
+                </Router>
             </div>
         );
     }
