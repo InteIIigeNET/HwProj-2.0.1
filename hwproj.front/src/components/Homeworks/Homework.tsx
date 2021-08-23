eb8a4fff0f6036fe04c5e19bcff47426ceb32bb8
NikitaBabich26rus <56031723+NikitaBabich26rus@users.noreply.github.com>
2021-08-23 15:06:06 +0300

    - Add adaptive styles to course and homeworks (#73)

--- hwproj.front/src/components/Homeworks/Homework.tsx
+++ hwproj.front/src/components/Homeworks/Homework.tsx
@@ -59,6 +59,7 @@ export default class Homework extends React.Component<IHomeworkProps, IHomeworkS
                     <div>
                         <HomeworkTasks onDelete={() => this.props.onDeleteClick()} tasks={this.props.homework.tasks!} forStudent={this.props.forStudent} forMentor={this.props.forMentor} />
                         <Button
+                            style={{ marginTop: "10px" }}
                             size="small"
                             variant="contained"
                             color="primary"
