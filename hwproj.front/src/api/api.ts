2b9a6ec260db5a8843b255ba7f4b046644c7bf72
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-08-25 14:04:58 +0300

    fix

--- hwproj.front/src/api/api.ts
+++ hwproj.front/src/api/api.ts
@@ -1035,6 +1035,41 @@ export const AccountApiFetchParamCreator = function (configuration?: Configurati
         },
         /**
          * 
+         * @param {string} [tokenId] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        apiAccountGooglePost(tokenId?: string, options: any = {}): FetchArgs {
+            const localVarPath = `/api/Account/google`;
+            const localVarUrlObj = url.parse(localVarPath, true);
+            const localVarRequestOptions = Object.assign({ method: 'POST' }, options);
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication Bearer required
+            if (configuration && configuration.apiKey) {
+                const localVarApiKeyValue = typeof configuration.apiKey === 'function'
+					? configuration.apiKey("Authorization")
+					: configuration.apiKey;
+                localVarHeaderParameter["Authorization"] = localVarApiKeyValue;
+            }
+
+            if (tokenId !== undefined) {
+                localVarQueryParameter['tokenId'] = tokenId;
+            }
+
+            localVarUrlObj.query = Object.assign({}, localVarUrlObj.query, localVarQueryParameter, options.query);
+            // fix override query string Detail: https://stackoverflow.com/a/7517673/1077943
+            delete localVarUrlObj.search;
+            localVarRequestOptions.headers = Object.assign({}, localVarHeaderParameter, options.headers);
+
+            return {
+                url: url.format(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
+         * 
          * @param {InviteLecturerViewModel} [model] 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -1202,6 +1237,24 @@ export const AccountApiFp = function(configuration?: Configuration) {
         },
         /**
          * 
+         * @param {string} [tokenId] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        apiAccountGooglePost(tokenId?: string, options?: any): (fetch?: FetchAPI, basePath?: string) => Promise<ResultTokenCredentials> {
+            const localVarFetchArgs = AccountApiFetchParamCreator(configuration).apiAccountGooglePost(tokenId, options);
+            return (fetch: FetchAPI = portableFetch, basePath: string = BASE_PATH) => {
+                return fetch(basePath + localVarFetchArgs.url, localVarFetchArgs.options).then((response) => {
+                    if (response.status >= 200 && response.status < 300) {
+                        return response.json();
+                    } else {
+                        throw response;
+                    }
+                });
+            };
+        },
+        /**
+         * 
          * @param {InviteLecturerViewModel} [model] 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -1291,6 +1344,15 @@ export const AccountApiFactory = function (configuration?: Configuration, fetch?
         },
         /**
          * 
+         * @param {string} [tokenId] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        apiAccountGooglePost(tokenId?: string, options?: any) {
+            return AccountApiFp(configuration).apiAccountGooglePost(tokenId, options)(fetch, basePath);
+        },
+        /**
+         * 
          * @param {InviteLecturerViewModel} [model] 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -1356,6 +1418,17 @@ export class AccountApi extends BaseAPI {
      */
     public apiAccountGetUserDataGet(options?: any) {
         return AccountApiFp(this.configuration).apiAccountGetUserDataGet(options)(this.fetch, this.basePath);
+    }
+
+    /**
+     * 
+     * @param {string} [tokenId] 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof AccountApi
+     */
+    public apiAccountGooglePost(tokenId?: string, options?: any) {
+        return AccountApiFp(this.configuration).apiAccountGooglePost(tokenId, options)(this.fetch, this.basePath);
     }
 
     /**
@@ -2812,7 +2885,6 @@ export class CoursesApi extends BaseAPI {
      * @memberof CoursesApi
      */
     public apiCoursesCreatePost(model?: CreateCourseViewModel, options?: any) {
-        debugger
         return CoursesApiFp(this.configuration).apiCoursesCreatePost(model, options)(this.fetch, this.basePath);
     }
 
