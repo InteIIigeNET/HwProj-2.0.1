86e19129d062582cee1d424d66477c61066f94e2
Semyon Tagilcev <76883458+semtagg@users.noreply.github.com>
2021-09-03 16:16:55 +0300

    init (#86)

--- HwProj.Common/HwProj.Models/SolutionsService/SolutionViewModel.cs
+++ HwProj.Common/HwProj.Models/SolutionsService/SolutionViewModel.cs
@@ -7,6 +7,7 @@ namespace HwProj.Models.SolutionsService
     public class SolutionViewModel
     {
         [Required]
+        [RegularExpression(@"^\S+.*", ErrorMessage = "Name shouldn't start with white spaces.")]
         public string GithubUrl { get; set; }
         
         public string Comment { get; set; }
