9eb2f79b96a8d877b9ad78b03701db527f69c41e
Charlotte000 <ramil.mifteev@yandex.ru>
2021-09-09 22:39:58 +0300

    fix accept lecturer

--- HwProj.CoursesService/HwProj.CoursesService.API/Startup.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 namespace HwProj.CoursesService.API
 {
