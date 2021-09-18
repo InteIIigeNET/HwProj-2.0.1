22ea2df66dd51c4786c4cd7545b1beedec1ca73d
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-09-18 20:31:45 +0300

    wip

--- hwproj.front/src/components/Groups/GroupStudents.tsx
+++ hwproj.front/src/components/Groups/GroupStudents.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import React, { FC } from 'react'
 import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 import Typography from '@material-ui/core/Typography'
