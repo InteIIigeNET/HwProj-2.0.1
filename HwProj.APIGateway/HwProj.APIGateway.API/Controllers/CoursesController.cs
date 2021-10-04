b89fc24a91de77e102841b1f8250e7b030c817f0
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2021-10-04 22:36:27 +0300

    attribute corrected

--- HwProj.APIGateway/HwProj.APIGateway.API/Controllers/CoursesController.cs
+++ HwProj.APIGateway/HwProj.APIGateway.API/Controllers/CoursesController.cs
@@ -26,7 +26,6 @@ namespace HwProj.APIGateway.API.Controllers
 
         [HttpGet]
         [ProducesResponseType(typeof(CourseViewModel[]), (int)HttpStatusCode.OK)]
-        [Authorize(Roles = Roles.LecturerRole)]
         public async Task<IActionResult> GetAllCourses()
         {
             var result = await _coursesClient.GetAllCourses();
