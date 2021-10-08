cbe321a6716ca16592eb2c03fdc4332e35231c51
Volodya Petrov <71704795+Volodya-Petrov@users.noreply.github.com>
2021-10-08 15:07:48 +0300

    GetAssessmentMethodForCourse added

--- HwProj.SolutionsService/HwProj.SolutionsService.API/AssessmentSystem/AssessmentSystem.cs
+++ HwProj.SolutionsService/HwProj.SolutionsService.API/AssessmentSystem/AssessmentSystem.cs
@@ -2,6 +2,8 @@ using System;
 using System.Linq;
 using System.Reflection;
 using ConfigurableAssessmentSystem;
+using HwProj.Models.SolutionsService;
+using HwProj.Models.StatisticsService;
 
 namespace HwProj.SolutionsService.API.AssessmentSystem
 {
@@ -9,15 +11,19 @@ namespace HwProj.SolutionsService.API.AssessmentSystem
     {
         public static string PathForAssessmentDlls { get; } = "../../../dllsForAssessment/dllForCourse";
         
-        public static Func<TaskModel, SolutionModel, int> GetAssessmentMethodForCourse(int courseId)
+        public static Func<TaskModel, SolutionModel[], int> GetAssessmentMethodForCourse(int courseId)
         {
-            var path = PathForAssessmentDlls + courseId.ToString() + ".dll";
+            var path = PathForAssessmentDlls + courseId + ".dll";
             var loadBuild = Assembly.LoadFrom(path);
             var classType = loadBuild.ExportedTypes.FirstOrDefault(t =>
                 t.IsClass && typeof(IAssessmentSystem).GetTypeInfo().IsAssignableFrom(t.GetTypeInfo()));
+            if (classType == null)
+            {
+                return null;
+            }
             var assessmentClass = (IAssessmentSystem)Activator.CreateInstance(classType);
-            return assessmentClass
-            
+            return assessmentClass.CalculateAssessmentForTask;
         }
+        
     }
 }
