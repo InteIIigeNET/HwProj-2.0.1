f5ef3676719e6e04cb62b9a7cd7f53880fc6a155
Volodya Petrov <89377379140@mail.ru>
2022-04-16 01:36:42 +0300

    init tests

---
+++ HwProj.CoursesService/CourseService.IntegrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using HwProj.CoursesService.API;
+using HwProj.CoursesService.API.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CourseService.IntegrationTests
+{
+    public class TestingWebAppFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<CourseContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                services.AddDbContext<CourseContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryEmployeeTest");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    using (var appContext = scope.ServiceProvider.GetRequiredService<CourseContext>())
+                    {
+                        appContext.Database.EnsureCreated();
+                    }
+                }
+            });
+        }
+    }
+}
