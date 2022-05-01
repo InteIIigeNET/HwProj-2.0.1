58969dd6fbf50030eaf9820d94aac74a120620f8
Volodya Petrov <89377379140@mail.ru>
2022-05-02 00:30:44 +0300

    wip

--- HwProj.CoursesService/HwProj.CoursesService.API/Controllers/CoursesController.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Controllers/CoursesController.cs
@@ -79,7 +79,7 @@ namespace HwProj.CoursesService.API.Controllers
             {
                 Name = courseViewModel.Name,
                 GroupName = courseViewModel.GroupName,
-                IsCompleted = courseViewModel.IsComplete,
+                IsCompleted = courseViewModel.IsCompleted,
                 IsOpen = courseViewModel.IsOpen
             });
 
