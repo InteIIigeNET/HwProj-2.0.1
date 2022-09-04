f9cdd77482608a289ea1b8bd91b8d56165e09dd2
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-09-04 05:15:11 +0300

    wip

--- HwProj.NotificationsService/HwProj.NotificationsService.API/Services/EmailService.cs
+++ HwProj.NotificationsService/HwProj.NotificationsService.API/Services/EmailService.cs
@@ -10,39 +10,39 @@ namespace HwProj.NotificationsService.API.Services
 {
     public class EmailService : IEmailService
     {
-        private readonly MailKit.Net.Smtp.SmtpClient _client;
+        private readonly IConfiguration _configuration;
         private readonly ILogger<Service> _logger;
-        private readonly string _serviceMail;
 
         public EmailService(IConfiguration configuration, ILogger<Service> logger)
         {
+            _configuration = configuration;
             _logger = logger;
-
-            var configurationSection = configuration.GetSection("Notification");
-            var connectSite = configurationSection["ConnectSite"];
-            var password = configurationSection["Password"];
-            _serviceMail = configurationSection["Mail"];
-
-            _client = new MailKit.Net.Smtp.SmtpClient();
-            _client.Connect(connectSite, 465, true);
-            _client.Authenticate(_serviceMail, password);
         }
 
         public async Task SendEmailAsync(Notification notification, string email, string topic)
         {
-            var emailMessage = new MimeMessage
-            {
-                From = { new MailboxAddress("HwProj-2.0.1", _serviceMail) },
-                To = { new MailboxAddress("", email) },
-                Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = notification.Body },
-                Subject = topic
-            };
-
-            _logger.LogInformation($"Sending email to {email} with topic {topic}");
-
             try
             {
-                await _client.SendAsync(emailMessage);
+                var configurationSection = _configuration.GetSection("Notification");
+                var connectSite = configurationSection["ConnectSite"];
+                var password = configurationSection["Password"];
+                var serviceMail = configurationSection["Mail"];
+
+                using var client = new MailKit.Net.Smtp.SmtpClient();
+                await client.ConnectAsync(connectSite, 465, true);
+                await client.AuthenticateAsync(serviceMail, password);
+
+                var emailMessage = new MimeMessage
+                {
+                    From = { new MailboxAddress("HwProj-2.0.1", serviceMail) },
+                    To = { new MailboxAddress("", email) },
+                    Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = notification.Body },
+                    Subject = topic
+                };
+
+                _logger.LogInformation($"Sending email to {email} with topic {topic}");
+
+                await client.SendAsync(emailMessage);
             }
             catch (Exception ex)
             {
