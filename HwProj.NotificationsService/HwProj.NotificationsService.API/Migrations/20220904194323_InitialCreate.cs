4cb29e66d42ae3e3bf95d9ec77e311b53cddcfcf
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-09-04 23:29:42 +0300

    wip

---
+++ HwProj.NotificationsService/HwProj.NotificationsService.API/Migrations/20220904194323_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HwProj.NotificationsService.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Sender = table.Column<string>(nullable: true),
+                    Owner = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    Body = table.Column<string>(nullable: true),
+                    HasSeen = table.Column<bool>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications");
+        }
+    }
+}
---
+++ HwProj.NotificationsService/HwProj.NotificationsService.API/Migrations/20220904194441_NotificationCategories.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using HwProj.Models.NotificationsService;
+using HwProj.NotificationsService.API.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HwProj.NotificationsService.API.Migrations
+{
+    public partial class NotificationCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Category",
+                table: "Notifications",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: false);
+
+            migrationBuilder.UpdateData(
+                table: "Notifications",
+                keyColumn: "Category",
+                keyValue: null,
+                column: "Category",
+                value: CategoryState.Profile);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
