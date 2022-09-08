aac0049896c89c4d413e043d4b5df8b5c25156b6
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-09-08 21:08:12 +0300

    SolutionStatisctics: fix for not accepted students

--- HwProj.SolutionsService/HwProj.SolutionsService.API/Domains/SolutionsStatsDomain.cs
+++ HwProj.SolutionsService/HwProj.SolutionsService.API/Domains/SolutionsStatsDomain.cs
@@ -7,30 +7,33 @@ namespace HwProj.SolutionsService.API.Domains
 {
     public static class SolutionsStatsDomain
     {
-        public static StatisticsCourseMatesModel[] GetCourseStatistics(StatisticsAggregateModel model)
-        {
-            return model.Course.CourseMates.Select( m => new StatisticsCourseMatesModel()
-            {
-                Id = m.StudentId,
-                Name = model.CourseMatesData.FirstOrDefault(kvp=>kvp.Key.Contains(m.StudentId)).Value.Name,
-                Surname = model.CourseMatesData.FirstOrDefault(kvp=>kvp.Key.Contains(m.StudentId)).Value.Surname,
-                Homeworks = new List<StatisticsCourseHomeworksModel>(model.Course.Homeworks.Select(h => new StatisticsCourseHomeworksModel()
+        //TODO: rewrite
+        public static StatisticsCourseMatesModel[] GetCourseStatistics(StatisticsAggregateModel model) =>
+            model.Course.CourseMates
+                .Where(t => t.IsAccepted)
+                .Select(m => new StatisticsCourseMatesModel
                 {
-                    Id = h.Id,
-                    Tasks = new List<StatisticsCourseTasksModel>(h.Tasks.Select(t =>
-                    {
-                        var solutions = model.Solutions.Where(s => s.TaskId == t.Id && s.StudentId == m.StudentId);
-                        var solutionsInRightModel =
-                            new List<StatisticsCourseSolutionsModel>(solutions.Select(s =>
-                                new StatisticsCourseSolutionsModel(s)));
-                        return new StatisticsCourseTasksModel()
+                    Id = m.StudentId,
+                    Name = model.CourseMatesData.FirstOrDefault(kvp => kvp.Key.Contains(m.StudentId)).Value.Name,
+                    Surname = model.CourseMatesData.FirstOrDefault(kvp => kvp.Key.Contains(m.StudentId)).Value.Surname,
+                    Homeworks = new List<StatisticsCourseHomeworksModel>(model.Course.Homeworks.Select(h =>
+                        new StatisticsCourseHomeworksModel
                         {
-                            Id = t.Id,
-                            Solution = solutionsInRightModel
-                        };
-                    }))
-                }))
-            }).ToArray();
-        }
+                            Id = h.Id,
+                            Tasks = new List<StatisticsCourseTasksModel>(h.Tasks.Select(t =>
+                            {
+                                var solutions =
+                                    model.Solutions.Where(s => s.TaskId == t.Id && s.StudentId == m.StudentId);
+                                var solutionsInRightModel =
+                                    new List<StatisticsCourseSolutionsModel>(solutions.Select(s =>
+                                        new StatisticsCourseSolutionsModel(s)));
+                                return new StatisticsCourseTasksModel()
+                                {
+                                    Id = t.Id,
+                                    Solution = solutionsInRightModel
+                                };
+                            }))
+                        }))
+                }).ToArray();
     }
 }
