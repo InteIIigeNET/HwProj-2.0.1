f34442f672c86d6fafc53966d810931db742ba32
Pavel Mokeev <p.mokeev@skbkontur.ru>
2023-04-04 15:57:15 +0300

    bugfix: Исправил ссылку на задание в уведомлении

--- HwProj.NotificationsService/HwProj.NotificationsService.API/EventHandlers/UpdateTaskMaxRatingEventHandler.cs
+++ HwProj.NotificationsService/HwProj.NotificationsService.API/EventHandlers/UpdateTaskMaxRatingEventHandler.cs
@@ -43,7 +43,7 @@ namespace HwProj.NotificationsService.API.EventHandlers
                 var notification = new Notification
                 {
                     Sender = "CourseService",
-                    Body = $"Задача <a href='{_configuration["Url"]}task/{@event.Task.Id}'>{@event.Task.Title}</a>" +
+                    Body = $"Задача <a href='{_configuration["Url"]}/task/{@event.Task.Id}'>{@event.Task.Title}</a>" +
                            $" из курса <a href='{_configuration["Url"]}courses/{@event.Course.Id}'>{@event.Course.Name}</a> обновлена.",
                     Category = CategoryState.Courses,
                     Date = DateTimeUtils.GetMoscowNow(),
