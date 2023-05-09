86ccef4748a0e877419872a22b02bdfbf74b5b2d
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-05-09 08:19:55 +0300

    StudentSolutionsPage: optimize requests

--- HwProj.APIGateway/HwProj.APIGateway.API/Controllers/SolutionsController.cs
+++ HwProj.APIGateway/HwProj.APIGateway.API/Controllers/SolutionsController.cs
@@ -13,6 +13,7 @@ using HwProj.Models.CoursesService.DTO;
 using HwProj.Models.CoursesService.ViewModels;
 using HwProj.Models.Roles;
 using HwProj.Models.SolutionsService;
+using HwProj.Models.StatisticsService;
 using HwProj.SolutionsService.Client;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -254,6 +255,29 @@ namespace HwProj.APIGateway.API.Controllers
                     };
                 }).ToArray()
             };
+
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet("courses/{courseId}/task/{taskId}")]
+        [ProducesResponseType(typeof(UserTaskSolutionPreviews[]), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCourseTaskSolutionsPageData(long courseId, long taskId)
+        {
+            var statistics = await _solutionsClient.GetCourseTaskStatistics(courseId, taskId, UserId);
+
+            var studentIds = statistics.Select(t => t.StudentId).ToArray();
+            var usersData = await AuthServiceClient.GetAccountsData(studentIds);
+
+            var result = statistics
+                .Zip(usersData, (statistic, accountData) => new UserTaskSolutionPreviews
+                {
+                    Solutions = statistic.Solutions.Select(s => new StatisticsCourseSolutionsModel(s)).ToArray(),
+                    User = accountData
+                })
+                .OrderBy(t => t.User.Surname)
+                .ThenBy(t => t.User.Name)
+                .ToArray();
 
             return Ok(result);
         }
