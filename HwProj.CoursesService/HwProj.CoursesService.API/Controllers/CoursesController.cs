9e57a81bc4f07a6ac219b53373e3202fda7ae71b
Maksim Tisheninov <stepmax14@gmail.com>
2023-07-13 13:18:08 +0300

    Minor tweaks

--- HwProj.CoursesService/HwProj.CoursesService.API/Controllers/CoursesController.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Controllers/CoursesController.cs
@@ -56,6 +56,7 @@ namespace HwProj.CoursesService.API.Controllers
             if (courseFromDb == null) return NotFound();
 
             var course = _mapper.Map<CourseDTO>(courseFromDb);
+            // TODO: Move group assignment to course repository or course service?
             course.Groups = await _groupsRepository.GetGroupsWithGroupMatesByCourse(course.Id).Select(g =>
                 new GroupViewModel
                 {
@@ -74,12 +75,14 @@ namespace HwProj.CoursesService.API.Controllers
             if (courseFromDb == null) return NotFound();
 
             var course = _mapper.Map<CourseDTO>(courseFromDb);
+            // TODO: Move group assignment to course repository or course service?
             course.Groups = await _groupsRepository.GetGroupsWithGroupMatesByCourse(course.Id).Select(g =>
                 new GroupViewModel
                 {
                     Id = g.Id,
                     StudentsIds = g.GroupMates.Select(t => t.StudentId).ToArray()
                 }).ToArrayAsync();
+            
             return Ok(course);
         }
 
