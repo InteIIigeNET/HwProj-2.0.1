8442eb78261398a10ff510f8a777331c6f23501e
Aleksey Poziev <aleksey.poziev@gmail.com>
2023-07-16 16:27:06 +0300

    feat: add better view for assign tab

--- hwproj.front/src/components/Courses/StudentsAssignment.tsx
+++ hwproj.front/src/components/Courses/StudentsAssignment.tsx
@@ -1,11 +1,11 @@
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import Stack from '@mui/material/Stack';
 import { AccountDataDto, CourseMateViewModel, CourseViewModel } from 'api';
 import ApiSingleton from 'api/ApiSingleton';
 import React from 'react';
 import {FC} from "react";
-
+import Grid from '@mui/material/Unstable_Grid2';
+import Box from '@mui/material/Box';
 
 interface IStudentAssignmentProps {
     course : CourseViewModel,
@@ -29,23 +29,37 @@ const StudentsAssignment : FC<IStudentAssignmentProps> = (props) => {
    
    return (
          <div>
-            <Stack>
+            <Box sx={{ flexGrow: 1, p: 2 }}>
+            <Grid 
+            container
+            spacing={2}
+            sx={{
+                '--Grid-borderWidth': '1px',
+                borderTop: 'var(--Grid-borderWidth) solid',
+                borderLeft: 'var(--Grid-borderWidth) solid',
+                borderColor: 'divider',
+                '& > div': {
+                    borderRight: 'var(--Grid-borderWidth) solid',
+                    borderBottom: 'var(--Grid-borderWidth) solid',
+                    borderColor: 'divider',
+                },
+            }}>
                 {props.mentors.map(current => 
+                    <Grid {...{ xs: 12, sm: 6, md: 4, lg: 5 }} minHeight={200} minWidth={300}>
                     <Autocomplete
                         disablePortal
                         id= " combo-box-demo"
                         options = {filterAssignedStudents(props.acceptedStudents, current.userId!)}
                         getOptionLabel= {(option) => createFullName(option)!}
                         onChange = {(event, value : AccountDataDto) => {
-                            alert(value.userId);
                             assignStudent(current.userId!, value.userId!)}}
                         sx ={{ width: 300 }}
                         renderInput={(params) => <TextField {...params} 
                         label = {createFullName(current)} />}                   
-                    />) //TODO : onchange event
+                    /> </Grid>) //TODO : onchange event
                 }
-                    
-            </Stack>
+            </Grid>
+            </Box>
          </div>
    )
 
