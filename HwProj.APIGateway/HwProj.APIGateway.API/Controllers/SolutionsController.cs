3402e0a4410bce0632c0558073dcb64c57a62ccd
Maksim Tisheninov <41760206+MaxLost@users.noreply.github.com>
2023-07-16 14:08:36 +0300

    API Gateway: remove GetAllSolutions (#259)

--- HwProj.APIGateway/HwProj.APIGateway.API/Controllers/SolutionsController.cs
+++ HwProj.APIGateway/HwProj.APIGateway.API/Controllers/SolutionsController.cs
@@ -10,7 +10,6 @@ using HwProj.CoursesService.Client;
 using HwProj.Models.CoursesService.ViewModels;
 using HwProj.Models.Roles;
 using HwProj.Models.SolutionsService;
-using HwProj.Models.StatisticsService;
 using HwProj.SolutionsService.Client;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -31,14 +30,6 @@ namespace HwProj.APIGateway.API.Controllers
         {
             _solutionsClient = solutionsClient;
             _coursesServiceClient = coursesServiceClient;
-        }
-
-        [HttpGet]
-        [ProducesResponseType(typeof(Solution[]), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> GetAllSolutions()
-        {
-            var result = await _solutionsClient.GetAllSolutions();
-            return Ok(result);
         }
 
         [HttpGet("{solutionId}")]
