558034bd3ef9937df28eb77bd00de3c448840c9c
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-07-17 23:14:33 +0300

    feat: Add AssignmentRepository to CourseAPI

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Repositories/AssignmentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using HwProj.CoursesService.API.Models;
+using HwProj.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HwProj.CoursesService.API.Repositories
+{
+    public class AssignmentsRepository: CrudRepository<Assignment, long>, IAssignmentsRepository
+    {
+        public AssignmentsRepository(CourseContext Context) : base(Context) { }
+
+        public async Task<Assignment[]> GetAllByCourseAsync(long courseId)
+        {
+            return await Context.Set<Assignment>()
+                .Include(a => a.StudentId)
+                .Include(a => a.MentorId)
+                .Where(a => a.CourseId == courseId)
+                .ToArrayAsync();
+        }
+    }
+}
