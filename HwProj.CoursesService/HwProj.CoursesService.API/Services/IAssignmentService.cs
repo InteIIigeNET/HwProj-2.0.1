e2699e9e0c681a8cceaa698ce3104676e1014f39
Aleksey Poziev <aleksey.poziev@gmail.com>
2023-07-17 23:31:11 +0300

    feat: add IAssignmentService

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Services/IAssignmentService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using HwProj.CoursesService.API.Models;
+
+namespace HwProj.CoursesService.API.Services;
+
+public interface IAssignmentService
+{
+    public Task AssignStudentAsync(string studentId, string mentorId, long courseId);
+
+    public Task DeassignStudentAsync(string studentId, long courseId);
+
+    public Task<Assignment[]> GetAllAssignmentsByCourseAsync(long courseId);
+
+}
