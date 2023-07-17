66fc6687b1b7d664343f5fd26053c9dd31e5df55
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-07-18 00:04:41 +0300

    feat: add previous methods

--- HwProj.CoursesService/HwProj.CoursesService.API/Services/AssignmentService.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Services/AssignmentService.cs
@@ -1,6 +1,35 @@
-﻿namespace HwProj.CoursesService.API.Services
+﻿using HwProj.CoursesService.API.Models;
+using HwProj.CoursesService.API.Repositories;
+using System.Threading.Tasks;
+
+namespace HwProj.CoursesService.API.Services
 {
-    public class AssignmentService
+    public class AssignmentService : IAssignmentService
     {
+        private readonly IAssignmentsRepository _assignmentRepository;
+
+        public AssignmentService(IAssignmentsRepository assignmentRepository)
+        {
+            _assignmentRepository = assignmentRepository;
+        }
+
+        public async Task AssignStudentAsync(string studentId, string mentorId, long courseId)
+        {
+        }
+
+        public async Task DeassignStudentAsync(string studentId, long courseId)
+        {
+            var student = _assignmentRepository.FindAsync(s => s.StudentId == studentId && s.CourseId == courseId).Result;
+
+            if (student != null)
+            {
+                await _assignmentRepository.DeleteAsync(student.Id);
+            }
+        }
+
+        public async Task<Assignment[]> GetAllAssignmentsByCourseAsync(long courseId)
+        {
+            return await _assignmentRepository.GetAllByCourseAsync(courseId);
+        }
     }
 }
