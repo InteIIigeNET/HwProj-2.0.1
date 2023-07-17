3b0e6f645626c7735e7134a43ef760b287eee9a5
Aleksey Poziev <aleksey.poziev@gmail.com>
2023-07-18 00:15:13 +0300

    refactor: from single to multiple form

--- HwProj.CoursesService/HwProj.CoursesService.API/Models/Assignment.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Assignment.cs
@@ -9,6 +9,8 @@ public class Assignment : IEntity<long>
     [Key] 
     public long Id { get; set; }
 
+    public long CourseId { get; set; }
+
     public string MentorId { get; set; }
 
     public string StudentId { get; set; }
