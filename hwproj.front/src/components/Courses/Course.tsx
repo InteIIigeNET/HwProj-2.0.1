361620a6f283621271078d851466886b9a42685c
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-07-24 02:20:13 +0500

    refactor

--- hwproj.front/src/components/Courses/Course.tsx
+++ hwproj.front/src/components/Courses/Course.tsx
@@ -340,7 +340,7 @@ const Course: React.FC = () => {
     
     useEffect(() => changeTab(tab || "homeworks"), [tab, courseId, isFound])
 
-    const userYandexId = new URLSearchParams(window.location.search).get("code")
+    const yandexCode = new URLSearchParams(window.location.search).get("code")
 
     const joinCourse = async () => {
         await ApiSingleton.coursesApi.coursesSignInCourse(+courseId!)
@@ -573,7 +573,7 @@ const Course: React.FC = () => {
                                     isMentor={isCourseMentor}
                                     course={courseState.course}
                                     solutions={studentSolutions}
-                                    yandexCode={userYandexId}
+                                    yandexCode={yandexCode}
                                 />
                             </Grid>
                         </Grid>}
