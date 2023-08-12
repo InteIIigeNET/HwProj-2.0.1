8fc7665d7cf0487ce7428b259038934d41782b31
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2023-08-12 22:10:21 +0300

    Update hwproj.front/src/services/Utils.ts

--- hwproj.front/src/services/Utils.ts
+++ hwproj.front/src/services/Utils.ts
@@ -1,5 +1,4 @@
 export default class Utils {
-
     static convertUTCDateToLocalDate(date: Date) {
         var newDate = new Date(new Date(date).getTime() - new Date(date).getTimezoneOffset() * 60 * 1000);
         return newDate;
