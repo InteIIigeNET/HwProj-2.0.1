40586e9e5a1352e07c5e5b4f35d5c9fa7172a0cb
Yuriy Kosarihin <yura9510@yandex.ru>
2023-08-12 22:03:22 +0300

    plural

--- hwproj.front/src/services/Utils.ts
+++ hwproj.front/src/services/Utils.ts
@@ -1,4 +1,5 @@
 export default class Utils {
+
     static convertUTCDateToLocalDate(date: Date) {
         var newDate = new Date(new Date(date).getTime() - new Date(date).getTimezoneOffset() * 60 * 1000);
         return newDate;
@@ -17,5 +18,22 @@ export default class Utils {
             'T' + pad(date.getHours()) +
             ':' + pad(date.getMinutes()) +
             ':' + pad(date.getSeconds())
+    }
+     static pluralize(diffTime : number) {
+         const diffHours = diffTime / (1000 * 60 * 60);
+         const diffDays = Math.trunc(diffHours / 24);
+         return diffDays === 0 ? Math.trunc(diffHours) + this.pluralizeHelper([" часов ", " часа ", " час "],diffHours) : diffDays + this.pluralizeHelper([" дней ", " дня ", " день "],diffDays)
+     }
+    static pluralizeHelper(forms: Array<string>, n : number) {
+        let idx;
+        if (n % 10 === 1 && n % 100 !== 11) {
+            idx = 0; // one
+        } else if (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)) {
+            idx = 1; // few
+        } else {
+            idx = 2; // many
+        }
+        return forms[idx] || '';
+
     }
 }
