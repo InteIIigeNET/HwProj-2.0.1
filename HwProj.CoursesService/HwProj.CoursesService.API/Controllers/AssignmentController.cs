d0e5fdcf16f802faf94b2ace8223d861e519b606
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-08-17 12:34:58 +0300

    feat: add Assignment controller

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Controllers/AssignmentController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using HwProj.CoursesService.API.Filters;
+using AutoMapper;
+using HwProj.CoursesService.API.Services;
+using HwProj.Models.CoursesService.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HwProj.CoursesService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentsController : Controller
+    {
+        private readonly IAssignmentsService _assignmentsService;
+        private IMapper _mapper;
+
+        public AssignmentsController(IAssignmentsService assignmentsService,
+            IMapper mapper)
+        {
+            _assignmentsService = assignmentsService;
+            _mapper = mapper;
+        }
+
+        [HttpPut("{courseId}/assignStudent")]
+        public async Task AssignStudentToMentor(long courseId, [FromQuery] string mentorId, [FromQuery] string studentId)
+        {
+            await _assignmentsService.AssignStudentAsync(studentId, mentorId, courseId);
+        }
+
+        [HttpDelete("{courseId}/deassignStudent")]
+        public async Task DeassignStudentFromMentor(long courseId, [FromQuery] string studentId)
+        {
+            await _assignmentsService.DeassignStudentAsync(studentId, courseId);
+        }
+    }
+}
