7fee4a6bdbdd0321c3b0881493188810195edef6
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-08-17 12:04:22 +0300

    feat: add AssignmentService and interface

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Services/IAssignmentService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using HwProj.CoursesService.API.Models;
+
+namespace HwProj.CoursesService.API.Services
+{
+    public interface IAssignmentsService
+    {
+        public Task AssignStudentAsync(string studentId, string mentorId, long courseId);
+
+        public Task DeassignStudentAsync(string studentId, long courseId);
+
+        public Task<Assignment[]> GetAllAssignmentsByCourseAsync(long courseId);
+
+    }
+}
