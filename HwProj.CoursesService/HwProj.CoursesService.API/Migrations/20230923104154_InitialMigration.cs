43c104f95bc29e6b5016a566fe4f216e4a4e570f
Aleksey Poziev <aleksey.poziev@gmail.com>
2023-09-23 13:49:26 +0300

    feat: add initial migration

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Migrations/20230923104154_InitialMigration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HwProj.CoursesService.API.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
