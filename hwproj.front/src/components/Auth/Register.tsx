786751a8e0393d509f6dda21c802c6c95fae10ee
Yuriy Kosarihin <yura9510@yandex.ru>
2023-09-26 16:42:45 +0300

    Добавлена проверка совпадения паролей на frontend

--- hwproj.front/src/components/Auth/Register.tsx
+++ hwproj.front/src/components/Auth/Register.tsx
@@ -56,8 +56,16 @@ const Register: FC<LoginProps> = (props) => {
         loggedIn: ApiSingleton.authService.isLoggedIn(),
         error: [],
     })
+    const [passwordError, setPasswordError] = useState<string>(""); // Состояние для ошибки паролей
+    const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false); // Состояние для блокировки кнопки
 
     const handleSubmit = async (e: any) => {
+        e.preventDefault();
+       if (registerState.password !== registerState.passwordConfirm) {
+            setPasswordError("Пароли не совпадают");
+            setIsButtonDisabled(true);
+            return;
+        }
         e.preventDefault()
         try {
             const result = await ApiSingleton.authService.register(registerState)
@@ -201,7 +209,11 @@ const Register: FC<LoginProps> = (props) => {
                                         ...prevState,
                                         passwordConfirm: e.target.value
                                     }))
+                                    setPasswordError("");
+                                    setIsButtonDisabled(false);
                                 }}
+                                error={passwordError !== ""}
+                                helperText={passwordError}
                             />
                         </Grid>
                     </Grid>
@@ -211,6 +223,7 @@ const Register: FC<LoginProps> = (props) => {
                         variant="contained"
                         color="primary"
                         type="submit"
+                        disabled={isButtonDisabled}
                     >
                         Зарегистрироваться
                     </Button>
