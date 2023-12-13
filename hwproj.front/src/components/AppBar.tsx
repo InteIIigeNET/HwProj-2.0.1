d6a86368195654264d3c18d87d38f523c40baee3
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-12-13 05:30:35 +0300

    Fix AppBar

--- hwproj.front/src/components/AppBar.tsx
+++ hwproj.front/src/components/AppBar.tsx
@@ -1,14 +1,12 @@
 import React, {useState} from "react";
 import {makeStyles} from '@material-ui/styles';
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import {Link} from "react-router-dom";
 import {Badge, Grid, IconButton, MenuItem, Typography} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import Menu from '@material-ui/core/Menu';
 import InviteLecturerModal from "./InviteLecturerModal";
 import MailIcon from '@mui/icons-material/Mail';
-import {useNavigate} from "react-router-dom";
 import {Button} from "@mui/material";
 
 const styles = makeStyles(theme => ({
@@ -61,119 +59,110 @@ export const Header: React.FC<AppBarProps> = (props: AppBarProps) => {
     return (
         <div>
             <AppBar style={{position: "static", width: "100vw", maxWidth: "100%"}}>
-                <div className={"container"}>
-                    <Toolbar>
-                        <Grid container spacing={1} alignItems={"center"}>
-                            <Grid item style={{marginRight: 1}}>
-                                <Link to={"/"}>
-                                    <Typography variant="h6" style={{color: 'white', fontFamily: "Helvetica"}}>
-                                        HW
-                                    </Typography>
+                <div className={"container"} style={{display: "flex"}}>
+                    <Grid container spacing={1} alignItems={"center"}>
+                        <Grid item>
+                            <Link to={"/"}>
+                                <Typography variant="h6" style={{color: 'white', fontFamily: "Helvetica"}}>
+                                    HW
+                                </Typography>
+                            </Link>
+                        </Grid>
+                        {props.loggedIn &&
+                            <Grid item>
+                                <Link to={"/notifications"}>
+                                    <IconButton>
+                                        {props.newNotificationsCount > 0
+                                            ? <Badge badgeContent={props.newNotificationsCount} color="primary">
+                                                <MailIcon fontSize={"small"} htmlColor={"white"}/>
+                                            </Badge>
+                                            : <MailIcon fontSize={"small"} htmlColor={"white"}/>
+                                        }
+                                    </IconButton>
                                 </Link>
                             </Grid>
-                            {props.loggedIn &&
-                                <Grid item>
-                                    <Link to={"/notifications"}>
-                                        <IconButton>
-                                            {props.newNotificationsCount > 0
-                                                ? <Badge badgeContent={props.newNotificationsCount} color="primary">
-                                                    <MailIcon fontSize={"small"} htmlColor={"white"}/>
-                                                </Badge>
-                                                : <MailIcon fontSize={"small"} htmlColor={"white"}/>
-                                            }
-                                        </IconButton>
-                                    </Link>
-                                </Grid>
-                            }
-                            {props.loggedIn &&
-                                <Grid item>
-                                    <Link
-                                        style={{color: 'white', fontFamily: "Helvetica", textDecoration: "none"}}
-                                        to={("/courses")}>
-                                        <Button>
-                                            <Typography style={{color: 'white', fontFamily: "Helvetica"}}>
-                                                Курсы
-                                            </Typography>
-                                        </Button>
-                                    </Link>
-                                </Grid>
-                            }
-                        </Grid>
-                        {props.loggedIn && props.isLecturer &&
-                            (
-                                <div>
-                                    <IconButton
-                                        edge="start"
-                                        color="inherit"
-                                        aria-label="menu"
-                                        onClick={handleClick}
-                                    >
-                                        <MenuIcon/>
-                                    </IconButton>
-                                    <Menu
-                                        id="simple-menu"
-                                        anchorEl={anchorEl}
-                                        keepMounted
-                                        open={Boolean(anchorEl)}
-                                        onClose={handleClose}
-                                    >
-                                        <MenuItem onClick={openInviteLecturer}>
-                                            Пригласить преподавателя
-                                        </MenuItem>
-                                        <Link
-                                            color={"initial"}
-                                            style={{textDecoration: "none"}}
-                                            to={"/create_course"}>
-                                            <MenuItem>
-                                                Создать курс
-                                            </MenuItem>
-                                        </Link>
-                                        <MenuItem onClick={props.onLogout}>
-                                            Выйти
-                                        </MenuItem>
-                                    </Menu>
-                                </div>
-                            )}
-                        {props.loggedIn && !props.isLecturer && (
-                            <div>
-                                <IconButton
-                                    edge="start"
-                                    color="inherit"
-                                    aria-label="menu"
-                                    onClick={handleClick}
-                                >
-                                    <MenuIcon/>
-                                </IconButton>
-                                <Menu
-                                    id="simple-menu"
-                                    anchorEl={anchorEl}
-                                    keepMounted
-                                    open={Boolean(anchorEl)}
-                                    onClose={handleClose}
-                                >
-                                    <MenuItem onClick={props.onLogout}>
-                                        Выйти
-                                    </MenuItem>
-                                </Menu>
-                            </div>
-                        )}
-                        {!props.loggedIn && (
-                            <div className={classes.tools}>
+                        }
+                        {props.loggedIn &&
+                            <Grid item>
                                 <Link
-                                    className={classes.item}
-                                    style={{color: "white", marginLeft: "10px"}}
-                                    to={("/login")}>
-                                    Вход
+                                    style={{color: 'white', fontFamily: "Helvetica", textDecoration: "none"}}
+                                    to={("/courses")}>
+                                    <Button>
+                                        <Typography style={{color: 'white', fontFamily: "Helvetica"}}>
+                                            Курсы
+                                        </Typography>
+                                    </Button>
                                 </Link>
-                                <Link
-                                    className={classes.item}
-                                    style={{color: "white", marginLeft: "10px"}}
-                                    to={"/register"}>
-                                    Регистрация
-                                </Link>
-                            </div>
-                        )}
-                    </Toolbar>
+                            </Grid>
+                        }
+                    </Grid>
+                    {props.loggedIn && props.isLecturer && <div>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            onClick={handleClick}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={openInviteLecturer}>
+                                Пригласить преподавателя
+                            </MenuItem>
+                            <Link
+                                color={"initial"}
+                                style={{textDecoration: "none"}}
+                                to={"/create_course"}>
+                                <MenuItem>
+                                    Создать курс
+                                </MenuItem>
+                            </Link>
+                            <MenuItem onClick={props.onLogout}>
+                                Выйти
+                            </MenuItem>
+                        </Menu>
+                    </div>}
+                    {props.loggedIn && !props.isLecturer && <div>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            onClick={handleClick}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={props.onLogout}>
+                                Выйти
+                            </MenuItem>
+                        </Menu>
+                    </div>}
+                    {!props.loggedIn && <div className={classes.tools}>
+                        <Link
+                            className={classes.item}
+                            style={{color: "white", marginLeft: "10px"}}
+                            to={("/login")}>
+                            Вход
+                        </Link>
+                        <Link
+                            className={classes.item}
+                            style={{color: "white", marginLeft: "10px"}}
+                            to={"/register"}>
+                            Регистрация
+                        </Link>
+                    </div>}
                 </div>
             </AppBar>
             {isOpenInviteLecturer && (
