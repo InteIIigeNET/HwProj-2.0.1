30c52104fb6d3df73aacb13470710c67948f1c10
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2024-01-08 23:46:13 +0300

    CoursesService: add migration for student assignments (#341)
    Co-authored-by: Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
    Co-authored-by: Egor Bakhorikov <117931736+BakhorikovEgor@users.noreply.github.com>

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Assignment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HwProj.Repositories;
+
+namespace HwProj.CoursesService.API.Models
+{
+    public class Assignment : IEntity<long>
+    {
+        [Key] public long Id { get; set; }
+
+        public long CourseId { get; set; }
+
+        public string MentorId { get; set; }
+
+        public string StudentId { get; set; }
+    }
+}
--- HwProj.CoursesService/HwProj.CoursesService.API/Models/Course.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Course.cs
@@ -6,8 +6,7 @@ namespace HwProj.CoursesService.API.Models
 {
     public class Course : IEntity<long>
     {
-        [Key]
-        public long Id { get; set; }
+        [Key] public long Id { get; set; }
         public string Name { get; set; }
         public string GroupName { get; set; }
         public bool IsOpen { get; set; }
@@ -16,5 +15,6 @@ namespace HwProj.CoursesService.API.Models
         public string MentorIds { get; set; }
         public List<CourseMate> CourseMates { get; set; } = new List<CourseMate>();
         public List<Homework> Homeworks { get; set; } = new List<Homework>();
+        public List<Assignment> Assignments { get; set; } = new List<Assignment>();
     }
 }
