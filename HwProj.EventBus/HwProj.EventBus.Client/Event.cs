3017b8f6497b7555e05ec32ce282ac6bdd29c291
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2024-02-17 01:51:23 +0300

    fix: style fixes

--- HwProj.EventBus/HwProj.EventBus.Client/Event.cs
+++ HwProj.EventBus/HwProj.EventBus.Client/Event.cs
@@ -5,9 +5,11 @@ namespace HwProj.EventBus.Client
 {
     public class Event
     {
-        [JsonProperty] public Guid Id { get; set; }
+        [JsonProperty] 
+        public Guid Id { get; set; }
 
-        [JsonProperty] public DateTime CreationData { get; set; }
+        [JsonProperty] 
+        public DateTime CreationData { get; set; }
 
         public Event()
         {
