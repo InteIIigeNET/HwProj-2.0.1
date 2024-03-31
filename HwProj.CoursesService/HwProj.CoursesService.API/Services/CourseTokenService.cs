aa748d74a15dd9497a4c063b316c6581769dfbd4
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2024-03-31 19:23:57 +0300

    namespace fix

--- HwProj.CoursesService/HwProj.CoursesService.API/Services/CourseTokenService.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Services/CourseTokenService.cs
@@ -12,56 +12,57 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HwProj.CoursesService.API.Services;
-
-public interface ICourseTokenService
+namespace HwProj.CoursesService.API.Services
 {
-    Task<Result<TokenCredentials>> GetTokenAsync(long courseId);
-}
-
-public class CourseTokenService : ICourseTokenService
-{
-    private readonly IConfigurationSection _configuration;
-    private readonly ICourseTokenRepository _courseTokenRepository;
-
-    public CourseTokenService(IConfiguration configuration, ICourseTokenRepository courseTokenRepository)
+    public interface ICourseTokenService
     {
-        _configuration = configuration.GetSection("AppSettings");
-        _courseTokenRepository = courseTokenRepository;
+        Task<Result<TokenCredentials>> GetTokenAsync(long courseId);
     }
-    
-    public async Task<Result<TokenCredentials>> GetTokenAsync(long courseId)
-    {
-        var courseToken = await _courseTokenRepository.GetAsync(courseId);
 
-        if (courseToken != null && courseToken.Expires > DateTime.UtcNow)
+    public class CourseTokenService : ICourseTokenService
+    {
+        private readonly IConfigurationSection _configuration;
+        private readonly ICourseTokenRepository _courseTokenRepository;
+
+        public CourseTokenService(IConfiguration configuration, ICourseTokenRepository courseTokenRepository)
         {
-            return Result<TokenCredentials>.Success(new TokenCredentials() { AccessToken = courseToken.Token });
+            _configuration = configuration.GetSection("AppSettings");
+            _courseTokenRepository = courseTokenRepository;
         }
 
-        await _courseTokenRepository.DeleteAsync(courseId);
+        public async Task<Result<TokenCredentials>> GetTokenAsync(long courseId)
+        {
+            var courseToken = await _courseTokenRepository.GetAsync(courseId);
 
-        var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["SecurityKey"]));
-        var expires = DateTime.Now.AddMinutes(int.Parse(_configuration["ExpiresIn"]));
-
-        var token = new JwtSecurityToken(
-            issuer: _configuration["ApiName"],
-            notBefore: DateTime.Now,
-            expires: expires,
-            claims: new[]
+            if (courseToken != null && courseToken.Expires > DateTime.UtcNow)
             {
+                return Result<TokenCredentials>.Success(new TokenCredentials() { AccessToken = courseToken.Token });
+            }
+
+            await _courseTokenRepository.DeleteAsync(courseId);
+
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["SecurityKey"]));
+            var expires = DateTime.Now.AddMinutes(int.Parse(_configuration["ExpiresIn"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["ApiName"],
+                notBefore: DateTime.Now,
+                expires: expires,
+                claims: new[]
+                {
                 new Claim("_course_Id", courseId.ToString()),
                 new Claim(ClaimTypes.Role, Roles.WorkflowRole)
-            },
-            signingCredentials: new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256));
+                },
+                signingCredentials: new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256));
 
-        var tokenCredentials = new TokenCredentials
-        {
-            AccessToken = new JwtSecurityTokenHandler().WriteToken(token)
-        };
+            var tokenCredentials = new TokenCredentials
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(token)
+            };
 
-        await _courseTokenRepository.AddAsync(new CourseToken() { CourseId = courseId, Token = tokenCredentials.AccessToken, Expires = expires });
+            await _courseTokenRepository.AddAsync(new CourseToken() { CourseId = courseId, Token = tokenCredentials.AccessToken, Expires = expires });
 
-        return Result<TokenCredentials>.Success(tokenCredentials);
+            return Result<TokenCredentials>.Success(tokenCredentials);
+        }
     }
 }
