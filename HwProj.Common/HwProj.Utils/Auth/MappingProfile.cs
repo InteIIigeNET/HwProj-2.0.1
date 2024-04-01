f11eb587eae0b1568841271963d5f935f6802478
YuriUfimtsev <ufimtsev3007@gmail.com>
2024-04-01 19:49:21 +0300

    add mapping options

---
+++ HwProj.Common/HwProj.Utils/Auth/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HwProj.Models.AuthService.ViewModels;
+
+namespace HwProj.Utils.Auth
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            CreateMap<RegisterExpertViewModel, User>()
+                .ForMember(destination => destination.Name,
+                    options => options.MapFrom(model => model.Name))
+                .ForMember(destination => destination.Surname,
+                    options => options.MapFrom(model => model.Surname))
+                .ForMember(destination => destination.MiddleName,
+                    options => options.MapFrom(model => model.MiddleName))
+                .ForMember(destination => destination.Email,
+                    options => options.MapFrom(model => model.Email))
+                .ForMember(destination => destination.Bio,
+                    options => options.MapFrom(model => model.Bio));
+        }
+    }
+}
