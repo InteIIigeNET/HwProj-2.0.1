befa32f24c71e5d7cbeb2bb824171062713aeca9
pavelrubanov <pavelpavelrubanov@gmail.com>
2024-04-06 22:24:26 +0300

    Migration created
    БД обновлена. Теперь есть таблчика Tag и TagMate.

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Migrations/20240406191646_AddTagsToHomeworks.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HwProj.CoursesService.API.Migrations
+{
+    public partial class AddTagsToHomeworks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagMates",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HomeworkId = table.Column<long>(nullable: false),
+                    TagId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagMates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TagMates_Homeworks_HomeworkId",
+                        column: x => x.HomeworkId,
+                        principalTable: "Homeworks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TagMates_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagMates_HomeworkId",
+                table: "TagMates",
+                column: "HomeworkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagMates_TagId",
+                table: "TagMates",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagMates");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
