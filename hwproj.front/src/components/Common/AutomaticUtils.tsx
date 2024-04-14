c9a87e3e25a18f3fdc09b0edaf3e8cbc2518aed6
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2024-04-14 12:00:23 +0300

    wip

--- hwproj.front/src/components/Common/AutomaticUtils.tsx
+++ hwproj.front/src/components/Common/AutomaticUtils.tsx
@@ -1,7 +1,7 @@
 export default class AutomaticUtils {
     static getAutoSendSolutionScript = (taskId: number) => {
-        return `
-response=$(curl -o /dev/null -s -w "%{http_code}" -X POST \\
+        return (
+`response=$(curl -o /dev/null -s -w "%{http_code}" -X POST \\
 ${window.location.origin}/api/automatic \\
 -H 'Content-Type: application/json' \\
 -H "Authorization: Bearer $TOKEN" \\
@@ -15,6 +15,5 @@ if [[ "$response" =~ ^2 ]]; then
 else
     echo "Request failed with status $response"
     exit 1
-fi`
-    }
+fi`)}
 }
