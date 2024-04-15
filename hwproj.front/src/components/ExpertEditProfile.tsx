b88f30606fbb835ed16842e85167ea1a90f3ebe6
YuriUfimtsev <ufimtsev3007@gmail.com>
2024-04-15 17:15:49 +0300

    wip

---
+++ hwproj.front/src/components/ExpertEditProfile.tsx
@@ -0,0 +1,25 @@
+﻿import * as React from "react";
+import {FC, FormEvent, useEffect, useState} from "react";
+import makeStyles from "@material-ui/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(3),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+    },
+    form: {
+        marginTop: theme.spacing(3),
+        width: '100%'
+    },
+}))
+
+const ExpertEditProfile: FC = () => {
+    return null;
+}
+
+export default ExpertEditProfile;
