bd274624a3682b8bc69a551c7e6123a35c1958c7
Niksen111 <nikiksen2003@gmail.com>
2024-04-29 01:51:52 +0300

    CourseService: Added JsonFormatSettings for CourseFilter

--- HwProj.CoursesService/HwProj.CoursesService.API/Models/Filter.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Filter.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace HwProj.CoursesService.API.Models
 {
     public class Filter
     {
+        [JsonProperty(PropertyName = "CM")]
         public List<string> CourseMateIds { get; set; }
+        [JsonProperty(PropertyName = "HW")]
         public List<long> HomeworkIds { get; set; }
+        [JsonProperty(PropertyName = "AS")]
         public List<long> AssignmenIds { get; set; }
     }
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
