a2a70a0f7cb513082b9e1a7bc668c29949e8fdb2
Niksen111 <nikiksen2003@gmail.com>
2024-04-29 01:10:42 +0300

    CourseService: Fixed CourseFilterService and Repository implementations mapping

--- HwProj.CoursesService/HwProj.CoursesService.API/Repositories/CourseFilterRepository.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Repositories/CourseFilterRepository.cs
@@ -1,12 +1,11 @@
 using HwProj.CoursesService.API.Models;
 using HwProj.Repositories;
-using Microsoft.EntityFrameworkCore;
 
 namespace HwProj.CoursesService.API.Repositories
 {
     public class CourseFilterRepository : CrudRepository<CourseFilter, long>, ICourseFilterRepository
     {
-        public CourseFilterRepository(DbContext context) : base(context)
+        public CourseFilterRepository(CourseContext context) : base(context)
         {
         }
     }
