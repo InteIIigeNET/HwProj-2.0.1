d72cf139a44676f0c90ab04e2542272e237b13c8
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2024-05-10 21:38:53 +0300

    feat: add impl for window

---
+++ hwproj.front/src/components/Common/TextWindow.tsx
@@ -0,0 +1,89 @@
+import React, {useState} from 'react';
+import {Box, Modal, Typography, IconButton, TextField} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import EditOffIcon from '@mui/icons-material/EditOff';
+import EditIcon from '@mui/icons-material/Edit';
+import {prism} from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+interface ITextWindowProps {
+    open: boolean;
+    text: string;
+    language: string;
+    onClose: () => void;
+    title?: string;
+}
+
+interface ITextWindowState {
+    text: string;
+    isEditMode: boolean;
+}
+
+const TextWindow: React.FC<ITextWindowProps> = (props) => {
+    const [state, setTextWindowState] = useState<ITextWindowState>({
+        text: props.text,
+        isEditMode: false
+    });
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(state.text);
+    };
+
+    return (
+        <Modal open={props.open} onClose={props.onClose}>
+            <Box
+                sx={{
+                    overflow: 'auto',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    bgcolor: 'background.paper',
+                    boxShadow: 24,
+                    p: 4,
+                    minWidth: 400,
+                    maxWidth: 650,
+                    minHeight: 400,
+                    maxHeight: 600
+                }}
+            >
+                <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
+                    <Typography variant="h6">{props.title ? props.title : props.language}</Typography>
+                    <Box>
+                        {state.isEditMode
+                            ? <IconButton color="primary" onClick={() => setTextWindowState({...state, isEditMode: false})}>
+                                <EditOffIcon/>
+                            </IconButton>
+                            : <IconButton color="primary" onClick={() => setTextWindowState({...state, isEditMode: true})}>
+                                <EditIcon/>
+                            </IconButton>}
+                        <IconButton onClick={handleCopy}>
+                            <ContentCopyIcon/>
+                        </IconButton>
+                        <IconButton onClick={props.onClose}>
+                            <CloseIcon/>
+                        </IconButton>
+                    </Box>
+                </Box>
+                <Box flexGrow={1} mb={2}>
+                    {state.isEditMode
+                        ? <TextField
+                            fullWidth
+                            variant="filled"
+                            multiline
+                            value={state.text}
+                            onChange={(e) => setTextWindowState({...state, text: e.target.value})}
+                            minRows={10}
+                            maxRows={15}
+                        />
+                        : <SyntaxHighlighter style={prism} language={props.language}>
+                            {state.text}
+                        </SyntaxHighlighter>}
+                </Box>
+            </Box>
+        </Modal>
+    );
+};
+
+export default TextWindow;
