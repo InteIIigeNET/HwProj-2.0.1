88524114a64e05f6b94a8bc3254754c7c12edc5b
Niksen111 <nikiksen2003@gmail.com>
2024-05-13 22:40:02 +0300

    CourseFilters: filter fixes

--- HwProj.CoursesService/HwProj.CoursesService.API/Models/Filter.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Models/Filter.cs
@@ -5,9 +5,12 @@ namespace HwProj.CoursesService.API.Models
 {
     public class Filter
     {
-        [JsonProperty(PropertyName = "CM")]
-        public List<string> CourseMateIds { get; set; }
-        [JsonProperty(PropertyName = "HW")]
-        public List<long> HomeworkIds { get; set; }
+        [JsonProperty(PropertyName = "STUD")] 
+        public List<string> StudentIds { get; set; } = new List<string>();
+
+        [JsonProperty(PropertyName = "HMW")] 
+        public List<long> HomeworkIds { get; set; } = new List<long>();
+        [JsonProperty(PropertyName = "MENT")]
+        public List<string> MentorIds { get; set; } = new List<string>();
     }
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
