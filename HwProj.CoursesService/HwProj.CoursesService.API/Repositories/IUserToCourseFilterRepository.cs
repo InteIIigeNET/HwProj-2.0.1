1b6c9752b3ad58e861df27ec6a5f3e4a2da52035
Niksen111 <nikiksen2003@gmail.com>
2024-05-13 22:40:26 +0300

    CourseFilters: Added UserToCourseFilter methods

--- HwProj.CoursesService/HwProj.CoursesService.API/Repositories/IUserToCourseFilterRepository.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Repositories/IUserToCourseFilterRepository.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using HwProj.CoursesService.API.Models;
 
@@ -6,7 +10,9 @@ namespace HwProj.CoursesService.API.Repositories
     public interface IUserToCourseFilterRepository
     {
         Task<UserToCourseFilter> GetAsync(string userId, long courseId);
+        IQueryable<UserToCourseFilter> FindAll(Expression<Func<UserToCourseFilter, bool>> predicate);
         Task AddAsync(UserToCourseFilter userToCourseFilter);
+        public Task AddAllAsync(IEnumerable<UserToCourseFilter> entities);
         Task UpdateAsync(UserToCourseFilter userToCourseFilter);
         Task DeleteAsync(string userId, long courseId);
     }
--- HwProj.CoursesService/HwProj.CoursesService.API/Repositories/UserToCourseFilterRepository.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Repositories/UserToCourseFilterRepository.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using HwProj.CoursesService.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -10,14 +13,24 @@ namespace HwProj.CoursesService.API.Repositories
     {
         private readonly CourseContext _context;
         
-        UserToCourseFilterRepository(CourseContext context)
+        public UserToCourseFilterRepository(CourseContext context)
         {
             _context = context;
         }
         
         public async Task<UserToCourseFilter> GetAsync(string userId, long courseId)
         {
-            return await _context.FindAsync<UserToCourseFilter>(userId, courseId).ConfigureAwait(false);
+            return await _context.FindAsync<UserToCourseFilter>(courseId, userId).ConfigureAwait(false);
+        }
+
+        public async Task AddAllAsync(IEnumerable<UserToCourseFilter> entities)
+        { 
+            await _context.AddRangeAsync(entities);
+        }
+
+        public IQueryable<UserToCourseFilter> FindAll(Expression<Func<UserToCourseFilter, bool>> predicate)
+        {
+            return _context.Set<UserToCourseFilter>().AsNoTracking().Where(predicate);
         }
 
         public async Task AddAsync(UserToCourseFilter userToCourseFilter)
