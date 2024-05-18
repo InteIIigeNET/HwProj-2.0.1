d3ad65c6d1af37212833bc468db49a5c815c9650
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2024-05-18 20:49:54 +0300

    add migration

--- HwProj.SolutionsService/HwProj.SolutionsService.API/Migrations/20240518174639_GithubSolutionCommits.Designer.cs
+++ HwProj.SolutionsService/HwProj.SolutionsService.API/Migrations/20240518174639_GithubSolutionCommits.Designer.cs
@@ -4,14 +4,16 @@ using HwProj.SolutionsService.API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HwProj.SolutionsService.API.Migrations
 {
     [DbContext(typeof(SolutionContext))]
-    partial class SolutionContextModelSnapshot : ModelSnapshot
+    [Migration("20240518174639_GithubSolutionCommits")]
+    partial class GithubSolutionCommits
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
