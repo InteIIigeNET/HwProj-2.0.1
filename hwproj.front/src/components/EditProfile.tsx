117d5c88b8cdcd6d172c26b8cfc00f4b410edfb3
YuriUfimtsev <ufimtsev3007@gmail.com>
2024-05-20 22:08:19 +0300

    refactor

--- hwproj.front/src/components/EditProfile.tsx
+++ hwproj.front/src/components/EditProfile.tsx
@@ -65,19 +65,6 @@ const EditProfile: FC = () => {
             middleName: profile.middleName,
         }
         try {
-            if (profile.isExternalAuth) {
-                const result = await ApiSingleton.accountApi.apiAccountEditPut(editForm)
-                result.succeeded
-                    ? setProfile((prevState) => ({
-                        ...prevState,
-                        edited: true,
-                    }))
-                    : setProfile((prevState) => ({
-                        ...prevState,
-                        errors: result.errors!
-                    }))
-                return
-            }
             const result = await ApiSingleton.accountApi.apiAccountEditPut(editForm)
             result.succeeded
                 ? setProfile((prevState) => ({
