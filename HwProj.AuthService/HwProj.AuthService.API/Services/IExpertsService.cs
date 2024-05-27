7ef92af4b501600ce5c60c4cb5a512c727a0e0c1
YuriUfimtsev <ufimtsev3007@gmail.com>
2024-05-27 23:16:06 +0300

    refactor: create IExpertsService

---
+++ HwProj.AuthService/HwProj.AuthService.API/Services/IExpertsService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using HwProj.Models.AuthService.DTO;
+using HwProj.Models.AuthService.ViewModels;
+using HwProj.Models.Result;
+
+namespace HwProj.AuthService.API.Services
+{
+    public interface IExpertsService
+    {
+        Task<Result> RegisterExpertAsync(RegisterExpertViewModel model, string lecturerId);
+        Task<Result<bool>> GetIsExpertProfileEdited(string expertAccountId);
+        Task<Result> SetExpertProfileIsEdited(string expertAccountId);
+        Task<Result> LoginExpertAsync(TokenCredentials tokenCredentials);
+        Task<ExpertDataDTO[]> GetAllExperts();
+        Task<Result> UpdateExpertTags(string lecturerId, UpdateExpertTagsDTO updateExpertTagsDto);
+    }
+}
