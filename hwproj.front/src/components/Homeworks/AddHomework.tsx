45f65f9ba3c2519765b59b8a9e461bc083c1c7d8
Trefex-trefex <125389570+Trefex-trefex@users.noreply.github.com>
2024-08-02 20:28:25 +0300

    Add info alert with the test work tag (#416)
    * Add info alert
    
    * fix the info alert
    
    * fix info alert

--- hwproj.front/src/components/Homeworks/AddHomework.tsx
+++ hwproj.front/src/components/Homeworks/AddHomework.tsx
@@ -6,8 +6,9 @@ import {TextFieldWithPreview} from "../Common/TextFieldWithPreview";
 import {CreateHomeworkViewModel, CreateTaskViewModel} from "../../api";
 import PublicationAndDeadlineDates from "../Common/PublicationAndDeadlineDates";
 import CreateTask from "../Tasks/CreateTask"
-import {Checkbox, FormControlLabel} from "@mui/material";
+import {Alert, Checkbox, FormControlLabel} from "@mui/material";
 import Tags from "./HomeworkTags";
+import { TestTag } from "components/Common/HomeworkTags";
 
 interface IAddHomeworkProps {
     id: number;
@@ -26,6 +27,7 @@ interface IAddHomeworkState {
     isDeadlineStrict: boolean;
     isGroupWork: boolean;
     hasErrors: boolean;
+    tags: string[];
 }
 
 interface IAddHomeworkTaskState {
@@ -55,7 +57,8 @@ const AddHomework: React.FC<IAddHomeworkProps> = (props) => {
         deadlineDate: undefined,
         isDeadlineStrict: false,
         added: false,
-        hasErrors: false
+        hasErrors: false,
+        tags: []
     })
 
     const handleSubmit = async (e: any) => {
@@ -120,6 +123,7 @@ const AddHomework: React.FC<IAddHomeworkProps> = (props) => {
                     }
                 />
                 <Tags tags={[]} courseId={props.id} onTagsChange={handleTagsChange}/>
+                {addHomeworkState.tags.includes(TestTag) && <Alert severity="info">Вы можете сгруппировать контрольные работы и переписывания с помощью дополнительного тега. Например, 'КР 1'</Alert>}
                 <PublicationAndDeadlineDates
                     hasDeadline={false}
                     isDeadlineStrict={false}
