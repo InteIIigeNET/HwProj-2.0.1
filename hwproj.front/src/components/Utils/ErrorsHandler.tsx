a0d2003429164fcd51224cc1df92e934715c1d79
YuriUfimtsev <ufimtsev3007@gmail.com>
2024-10-10 03:00:19 +0300

    [frontend] implement ErrorsHandler util with getErrorMessages method that shows backend error or 'Сервис недоступен'

--- hwproj.front/src/components/Utils/ErrorsHandler.tsx
+++ hwproj.front/src/components/Utils/ErrorsHandler.tsx
@@ -1,20 +1,10 @@
 ﻿export default class ErrorsHandler {
-    static defaultErrorMessage = 'Сервис недоступен';
-
-    static isStringNullOrEmpty(str: string | undefined): boolean {
-        return str === undefined || str.trim().length === 0;
-    }
-
-    static async getErrorMessages(response: Response): Promise<string[]> {
+    static async getErrorMessages(response : Response) : Promise<string[]> {
         try {
             const message = await response.text();
-            if (this.isStringNullOrEmpty(message)) {
-                return [this.defaultErrorMessage];
-            }
-
             return [message];
         } catch {
-            return [this.defaultErrorMessage];
+            return ['Сервис недоступен'];
         }
     }
 }
