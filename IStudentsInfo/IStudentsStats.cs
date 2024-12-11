c56fa2cc714b0545f0e39caee07de13957ce2613
Pavel Averin <pasha2281337alfa@gmail.com>
2024-12-11 21:51:10 +0300

    feat: add ProgramNames property to interface

--- IStudentsInfo/IStudentsStats.cs
+++ IStudentsInfo/IStudentsStats.cs
@@ -5,13 +5,18 @@ namespace IStudentsInfo
     public interface IStudentsStats
     {
         /// <summary>
-        /// Given a name of a study program in English, returns a list of corresponding academic groups.
+        /// Given a name of a study program in English, returns a list of corresponding academic groups
         /// </summary>
         List<string> GetGroups(string programName);
 
         /// <summary>
-        /// Given a name of an academic group returns a list of students' STs (Student IDs).
+        /// Given a name of an academic group returns a list of students' STs (Student IDs)
         /// </summary>
         List<string> GetSts(string groupName);
+        
+        /// <summary>
+        /// Returns study program names
+        /// </summary>
+        List<string> ProgramNames { get; }
     }
 }
