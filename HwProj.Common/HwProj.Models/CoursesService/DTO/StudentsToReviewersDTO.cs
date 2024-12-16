7fc192876596c0f2272e0c60ef8053a6dec5068c
Yuri <ufimtsev3007@gmail.com>
2024-12-16 03:11:57 +0300

    [backend] implement students-lecturers assigning models: update CourseFilterRepository, CourseFilterService, StatisticsController; create new models and dto

---
+++ HwProj.Common/HwProj.Models/CoursesService/DTO/StudentsToReviewersDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using HwProj.Models.AuthService.DTO;
+
+namespace HwProj.Models.CoursesService.DTO
+{
+    public class StudentsToReviewersDTO
+    {
+        public Dictionary<string, AccountDataDto[]> StudentsToReviewersDictionary { get; set; } =
+            new Dictionary<string, AccountDataDto[]>();
+    }
+}
