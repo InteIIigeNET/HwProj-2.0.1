3da90f4d188c96d763c08837d467a1b627742886
bygu4 <sacha05bugaev@gmail.com>
2025-03-03 23:02:58 +0300

    update template models and mappings, support templates in CoursesController

--- HwProj.CoursesService/HwProj.CoursesService.API/Controllers/CoursesController.cs
+++ HwProj.CoursesService/HwProj.CoursesService.API/Controllers/CoursesController.cs
@@ -27,15 +27,21 @@ namespace HwProj.CoursesService.API.Controllers
     {
         private readonly ICoursesService _coursesService;
         private readonly ICourseFilterService _courseFilterService;
+        private readonly IHomeworksService _homeworksService;
+        private readonly ITasksService _tasksService;
         private readonly IHomeworksRepository _homeworksRepository;
         private readonly IMapper _mapper;
 
         public CoursesController(ICoursesService coursesService,
+            IHomeworksService homeworksService,
+            ITasksService tasksService,
             IHomeworksRepository homeworksRepository,
             IMapper mapper,
             ICourseFilterService courseFilterService)
         {
             _coursesService = coursesService;
+            _homeworksService = homeworksService;
+            _tasksService = tasksService;
             _homeworksRepository = homeworksRepository;
             _mapper = mapper;
             _courseFilterService = courseFilterService;
@@ -242,6 +248,32 @@ namespace HwProj.CoursesService.API.Controllers
             result = result.Concat(defaultTags).Distinct().ToArray();
 
             return Ok(result);
+        }
+
+        [HttpPost("recreate/{courseId}")]
+        public async Task<IActionResult> RecreateCourse(long courseId, [FromQuery] string mentorId)
+        {
+            var course = await _coursesService.GetAsync(courseId, mentorId);
+            if (course == null) return NotFound();
+
+            var courseTemplate = course.ToCourseTemplate();
+
+            return await AddCourseFromTemplate(courseTemplate, mentorId);
+        }
+
+        private async Task<IActionResult> AddCourseFromTemplate(CourseTemplate courseTemplate, string mentorId)
+        {
+            var courseId = await _coursesService.AddAsync(courseTemplate.ToCourse(), mentorId);
+            foreach (var homeworkTemplate in courseTemplate.Homeworks)
+            {
+                var homeworkId = await _homeworksService.AddHomeworkAsync(courseId, homeworkTemplate.ToHomework());
+                foreach (var taskTemplate in homeworkTemplate.Tasks)
+                {
+                    await _tasksService.AddTaskAsync(homeworkId, taskTemplate.ToHomeworkTask());
+                }
+            }
+
+            return Ok(courseId);
         }
 
         [HttpGet("getMentorsToStudents/{courseId}")]
