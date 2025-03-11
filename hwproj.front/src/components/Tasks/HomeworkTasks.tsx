9ad7a8a6bfad707bf3141674d7c24ea867f45543
Yuri Ufimtsev <91129832+YuriUfimtsev@users.noreply.github.com>
2025-03-12 01:29:50 +0300

    fix tasks ordering (#517)

--- hwproj.front/src/components/Tasks/HomeworkTasks.tsx
+++ hwproj.front/src/components/Tasks/HomeworkTasks.tsx
@@ -31,7 +31,7 @@ const HomeworkTasks: FC<IHomeworkTasksProps> = (props) => {
                             />
                         </div>
                     )
-                }).reverse()}
+                })}
         </div>
     )
 }
