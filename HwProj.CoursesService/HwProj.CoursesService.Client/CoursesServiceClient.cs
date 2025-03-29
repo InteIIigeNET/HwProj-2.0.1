b92db0a4ed1790c91671f8cbe49e871c218193a3
bygu4 <sacha05bugaev@gmail.com>
2025-03-29 13:59:18 +0300

    use UserId from the request header

--- HwProj.CoursesService/HwProj.CoursesService.Client/CoursesServiceClient.cs
+++ HwProj.CoursesService/HwProj.CoursesService.Client/CoursesServiceClient.cs
@@ -124,13 +124,11 @@ namespace HwProj.CoursesService.Client
             return await response.DeserializeAsync<long>();
         }
 
-        public async Task<Result<long>> CreateCourseBasedOn(long courseId,
-            CreateCourseViewModel model,
-            string mentorId)
+        public async Task<Result<long>> CreateCourseBasedOn(long courseId, CreateCourseViewModel model)
         {
             using var httpRequest = new HttpRequestMessage(
                 HttpMethod.Post,
-                _coursesServiceUri + $"api/Courses/createBasedOn/{courseId}?mentorId={mentorId}")
+                _coursesServiceUri + $"api/Courses/createBasedOn/{courseId}")
             {
                 Content = new StringContent(
                     JsonConvert.SerializeObject(model),
