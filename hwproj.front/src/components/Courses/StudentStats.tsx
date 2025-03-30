379141ff88f1919bdf306014193cd505f8c36a50
Yuri Ufimtsev <91129832+YuriUfimtsev@users.noreply.github.com>
2025-03-30 16:46:34 +0300

    Улучшения UI (#536)
    * refactor: optimize solutions sorting in cells
    
    * add key props
    
    * remove required property from solution link

--- hwproj.front/src/components/Courses/StudentStats.tsx
+++ hwproj.front/src/components/Courses/StudentStats.tsx
@@ -279,10 +279,10 @@ const StudentStats: React.FC<IStudentStatsProps> = (props) => {
                                         homework.tasks!.map((task, i) => {
                                             const additionalStyles = i === 0 && homeworkStyles(homeworks, idx)
                                             return <StudentStatsCell
-                                                solutions={solutions
-                                                    .find(s => s.id === cm.id)!.homeworks!
-                                                    .find(h => h.id === homework.id)!.tasks!
-                                                    .find(t => t.id === task.id)!.solution!}
+                                                key={`${cm.id}-${task.id}`}
+                                                solutions={cm.homeworks
+                                                    ?.find(h => h.id === homework.id)?.tasks
+                                                    ?.find(t => t.id === task.id)?.solution || []}
                                                 userId={props.userId}
                                                 forMentor={props.isMentor}
                                                 studentId={String(cm.id)}
