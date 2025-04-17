6056a0f4d06e94321be8dd61a02ab869ce654805
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-04-18 01:59:34 +0300

    Yandex.Metrica: анонимизация идентификаторов в путях

--- hwproj.front/src/TrackPageChanges.tsx
+++ hwproj.front/src/TrackPageChanges.tsx
@@ -1,22 +1,35 @@
 import ApiSingleton from 'api/ApiSingleton';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
+
+const idRegex = /^\d+$/
+const uuidRegex = /^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$/i
 
 const TrackPageChanges = () => {
-    const location = useLocation();
-    
+    const location = useLocation()
+
+    const anonymizePath = (pathname: string) => {
+        if (pathname.startsWith('/join')) return "/join/TOKEN"
+        return pathname
+            .split('/')
+            .map(segment => idRegex.test(segment) || uuidRegex.test(segment)
+                ? 'ID'
+                : segment)
+            .join('/')
+    }
+
     useEffect(() => {
         // Отправляем данные о новом URL в Яндекс.Метрику
         if (window.ym) {
-            window.ym(101061418, 'hit', location.pathname, {
+            window.ym(101061418, 'hit', anonymizePath(location.pathname), {
                 params: {
                     user_role: ApiSingleton.authService.getRole()
                 }
-            });
+            })
         }
-    }, [location.pathname]);
+    }, [location.pathname])
 
-    return null;
-};
+    return null
+}
 
 export default TrackPageChanges;
