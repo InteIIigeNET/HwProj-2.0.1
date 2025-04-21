5e2df481371cc1b715ce4e32edcafb8dfa831746
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-04-21 03:55:02 +0300

    Характеристика: сворачивать длинный текст

--- hwproj.front/src/components/Students/StudentCharacteristics.tsx
+++ hwproj.front/src/components/Students/StudentCharacteristics.tsx
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+﻿import React, {useEffect, useState} from 'react';
 import {
     Grid,
     Chip,
@@ -10,7 +10,7 @@ import {
     DialogContent,
     Autocomplete,
     DialogActions,
-    AlertTitle
+    AlertTitle, Stack
 } from '@mui/material';
 import {StudentCharacteristicsDto} from '@/api';
 import TextField from "@material-ui/core/TextField";
@@ -38,23 +38,20 @@ const renderTag = (tag: string, props?: any) => {
 export const StudentCharacteristics: React.FC<Props> = (props) => {
     const {characteristics} = props
     const [isEdit, setIsEdit] = React.useState(false)
+    const description = characteristics?.description
     const hasCharacteristics = characteristics && (characteristics.tags && characteristics.tags.length > 0 || characteristics.description)
+    const maxDescriptionLength = 400
+    const tooLongDescription = description != undefined && description.length > maxDescriptionLength
+
+    const [showFullDescription, setShowFullDescription] = useState(!tooLongDescription)
+
+    useEffect(() => setShowFullDescription(!tooLongDescription), [tooLongDescription])
 
     const renderUpdateButton = () => {
         return <Typography color={"#3f51b5"} variant={"caption"} style={{cursor: "pointer"}}
                            onClick={() => setIsEdit(true)}>
             {hasCharacteristics ? "Изменить характеристику" : "Добавить характеристику студенту"}
         </Typography>
-    }
-
-    const comparer = (a: string, b: string) => {
-        if (a.startsWith("-")) {
-            return b.startsWith("-") ? 0 : 1
-        }
-        if (a.startsWith("+")) {
-            return b.startsWith("+") ? 0 : -1
-        }
-        return 0
     }
 
     if (!hasCharacteristics && !isEdit) return renderUpdateButton()
@@ -81,9 +78,20 @@ export const StudentCharacteristics: React.FC<Props> = (props) => {
                             </Grid>
                         ))}
                     </Grid>
-                    {characteristics?.description && <Grid item>
+                    {description && <Grid item
+                                          style={tooLongDescription ? {cursor: "pointer"} : undefined}
+                                          onClick={() => {
+                                              if (!showFullDescription) {
+                                                  setShowFullDescription(true)
+                                              } else if (showFullDescription && tooLongDescription) {
+                                                  setShowFullDescription(false)
+                                              }
+                                          }}>
                         <MarkdownPreview
-                            value={characteristics.description}
+                            value={showFullDescription
+                                ? tooLongDescription
+                                    ? description + "\n\n**Нажмите, чтобы свернуть**" : description
+                                : description.substring(0, maxDescriptionLength) + "...**Нажмите, чтобы показать целиком**"}
                         />
                     </Grid>}
                     <Grid item>{renderUpdateButton()}</Grid>
@@ -93,101 +101,104 @@ export const StudentCharacteristics: React.FC<Props> = (props) => {
         </div>)
 };
 
-const EditStudentCharacteristics: React.FC<Props & { onCancel: () => void, isOpen: boolean }> = (props) => {
-    const [characteristics, setCharacteristics] = React.useState<StudentCharacteristicsDto>(props.characteristics || {
-        tags: [],
-        description: ""
-    })
-    const handleSubmit = async () => {
-        await ApiSingleton.coursesApi.coursesUpdateStudentCharacteristics(props.courseId, props.studentId, characteristics)
-        props.onChange(characteristics)
+const EditStudentCharacteristics: React.FC<Props & { onCancel: () => void, isOpen: boolean }> =
+    (props) => {
+        const [characteristics, setCharacteristics] = React.useState<StudentCharacteristicsDto>(props.characteristics || {
+            tags: [],
+            description: ""
+        })
+        const handleSubmit = async () => {
+            await ApiSingleton.coursesApi.coursesUpdateStudentCharacteristics(props.courseId, props.studentId, characteristics)
+            props.onChange(characteristics)
+        }
+        return (
+            <Dialog
+                fullWidth
+                maxWidth="md"
+                open={props.isOpen}
+                onClose={() => props.onCancel()}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">
+                    Добавить характеристику студенту
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container direction={"column"} spacing={1}>
+                        <Grid item>
+                            <Alert severity="info">
+                                Студент не будет видеть характеристику, но она будет доступна другим преподавателям
+                                курса
+                            </Alert>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant={"body2"}>
+                                Добавьте перед характеристикой '-' для отрицательной оценки или '+' для
+                                положительной.
+                                <br/>
+                                Не добавляйте ничего, если хотите указать нейтральную характеристику.
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Autocomplete
+                                fullWidth
+                                multiple
+                                freeSolo
+                                id="tags-outlined"
+                                options={["+ Талантливый студент", "- Списывает", "Удален с курса"]}
+                                value={characteristics?.tags ?? []}
+                                defaultValue={characteristics?.tags ?? []}
+                                filterSelectedOptions
+                                onChange={(e, values) => {
+                                    e.persist()
+                                    setCharacteristics((prevState) => ({
+                                        ...prevState,
+                                        tags: values
+                                    }))
+                                }}
+                                renderTags={(values, getTagProps) => <div>
+                                    {values.map((value, index) => renderTag(value, getTagProps({index})))}
+                                </div>}
+                                renderInput={(params) => (
+                                    <TextField
+                                        {...params}
+                                        label="Характеристики"
+                                        placeholder="Напишите"
+                                    />
+                                )}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <MarkdownEditor
+                                label={"Подробное описание (опционально)"}
+                                value={characteristics?.description ?? ""}
+                                onChange={(value) => {
+                                    setCharacteristics((prevState) => ({
+                                        ...prevState,
+                                        description: value
+                                    }))
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        size="small"
+                        variant="text"
+                        color="primary"
+                        type="submit"
+                        onClick={handleSubmit}
+                    >
+                        Обновить
+                    </Button>
+                    <Button
+                        size="small"
+                        onClick={() => props.onCancel()}
+                        variant="text"
+                        color="error"
+                    >
+                        Закрыть
+                    </Button>
+                </DialogActions>
+            </Dialog>)
     }
-    return (
-        <Dialog
-            fullWidth
-            maxWidth="md"
-            open={props.isOpen}
-            onClose={() => props.onCancel()}
-            aria-labelledby="form-dialog-title"
-        >
-            <DialogTitle id="form-dialog-title">
-                Добавить характеристику студенту
-            </DialogTitle>
-            <DialogContent>
-                <Grid container direction={"column"} spacing={1}>
-                    <Grid item>
-                        <Alert severity="info">
-                            Студент не будет видеть характеристику, но она будет доступна другим преподавателям курса
-                        </Alert>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant={"body2"}>
-                            Добавьте перед характеристикой '-' для отрицательной оценки или '+' для положительной.
-                            <br/>
-                            Не добавляйте ничего, если хотите указать нейтральную характеристику.
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Autocomplete
-                            fullWidth
-                            multiple
-                            freeSolo
-                            id="tags-outlined"
-                            options={["+ Талантливый студент", "- Списывает", "Удален с курса"]}
-                            value={characteristics?.tags ?? []}
-                            defaultValue={characteristics?.tags ?? []}
-                            filterSelectedOptions
-                            onChange={(e, values) => {
-                                e.persist()
-                                setCharacteristics((prevState) => ({
-                                    ...prevState,
-                                    tags: values
-                                }))
-                            }}
-                            renderTags={(values, getTagProps) => <div>
-                                {values.map((value, index) => renderTag(value, getTagProps({index})))}
-                            </div>}
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    label="Характеристики"
-                                    placeholder="Напишите"
-                                />
-                            )}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <MarkdownEditor
-                            label={"Подробное описание (опционально)"}
-                            value={characteristics?.description ?? ""}
-                            onChange={(value) => {
-                                setCharacteristics((prevState) => ({
-                                    ...prevState,
-                                    description: value
-                                }))
-                            }}
-                        />
-                    </Grid>
-                </Grid>
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    size="small"
-                    variant="text"
-                    color="primary"
-                    type="submit"
-                    onClick={handleSubmit}
-                >
-                    Обновить
-                </Button>
-                <Button
-                    size="small"
-                    onClick={() => props.onCancel()}
-                    variant="text"
-                    color="error"
-                >
-                    Закрыть
-                </Button>
-            </DialogActions>
-        </Dialog>)
-}
