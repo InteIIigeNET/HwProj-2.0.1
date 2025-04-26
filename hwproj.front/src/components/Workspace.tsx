9e779c939d34bcf7b8230aa5c26ce641b40123f4
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-04-26 23:04:21 +0300

    Workspace: исправление стилей

--- hwproj.front/src/components/Workspace.tsx
+++ hwproj.front/src/components/Workspace.tsx
@@ -5,7 +5,6 @@ import {UnratedSolutionPreviews, UserDataDto} from "../api/";
 import "./Styles/Profile.css";
 import {FC, useEffect, useState} from "react";
 import {Link, useParams} from "react-router-dom";
-import {makeStyles} from "@material-ui/styles";
 import TaskDeadlines from "./Tasks/TaskDeadlines";
 import UnratedSolutions from "./Solutions/UnratedSolutions";
 import {Alert, Chip, Stack} from "@mui/material";
@@ -19,12 +18,6 @@ interface IWorkspaceState {
     isLoaded: boolean;
     tabValue: number;
 }
-
-const useStyles = makeStyles(() => ({
-    info: {
-        justifyContent: "space-between",
-    },
-}))
 
 const Workspace: FC = () => {
     const {id} = useParams()
@@ -41,7 +34,6 @@ const Workspace: FC = () => {
         unratedSolutionPreviews: undefined
     })
 
-    const classes = useStyles()
     const isLecturer = ApiSingleton.authService.isLecturer()
     const isExpert = ApiSingleton.authService.isExpert()
     const isMentor = isLecturer || isExpert
@@ -100,8 +92,8 @@ const Workspace: FC = () => {
         return (
             <div className="container" style={{marginBottom: '50px'}}>
                 <Grid container style={{marginTop: "15px"}} spacing={2}>
-                    <Grid item container className={classes.info} direction={"row"}>
-                        <Grid item direction={"row"} spacing={2} style={{display: "flex"}}>
+                    <Grid item container direction={"row"} justifyContent={"space-between"}>
+                        <Grid item direction={"row"} spacing={2}>
                             <Grid item>
                                 <Typography style={{fontSize: '20px'}}>
                                     {fullName}
