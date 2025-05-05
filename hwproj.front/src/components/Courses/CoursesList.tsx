72f5ae577f76158de0e71ce04e070b7f87aec89f
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-05-05 05:31:56 +0300

    CoursesList:  анимация загрузки

--- hwproj.front/src/components/Courses/CoursesList.tsx
+++ hwproj.front/src/components/Courses/CoursesList.tsx
@@ -1,24 +1,36 @@
 import * as React from "react";
-import {CoursePreviewView} from "../../api/";
+import {CoursePreviewView} from "@/api";
 import {NavLink} from "react-router-dom";
 import MentorsList from "../Common/MentorsList";
-import {Box, Divider, Grid, ListItem, Typography} from "@mui/material";
+import {Box, Divider, Grid, ListItem, Skeleton, Typography} from "@mui/material";
 import NameBuilder from "../Utils/NameBuilder";
 
 interface ICoursesProps {
     navigate: any
-    courses: CoursePreviewView[];
+    courses: CoursePreviewView[] | undefined;
     isExpert: boolean
 }
 
 export class CoursesList extends React.Component<ICoursesProps, {}> {
     public render() {
         const {courses, isExpert} = this.props;
+        const ghostCoursesRand = [45, 12, 23, 3, 67, 50, 19]
 
         return (
             <Box>
                 <Grid container direction="column">
-                    {courses.map((course, i) => (
+                    {courses === undefined && ghostCoursesRand.map((rand, i) => {
+                        return <Grid item key={i}>
+                            <Skeleton variant="rectangular" animation="wave" width={300 + rand} height={25}
+                                      style={{marginBottom: 5}}/>
+                            <Skeleton variant="rectangular" animation="wave" width={50 + rand} height={14}
+                                      style={{marginBottom: 10}}/>
+                            <Skeleton variant="rectangular" animation="wave" width={150 + rand} height={20}/>
+                            {i < ghostCoursesRand.length - 1 ?
+                                <Divider style={{marginTop: 10, marginBottom: 10}}/> : null}
+                        </Grid>;
+                    })}
+                    {courses && courses.map((course, i) => (
                         <Grid item key={course.id}>
                             <ListItem
                                 style={{padding: 0, marginBottom: 5}}
