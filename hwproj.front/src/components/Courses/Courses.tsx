5c3e0eb11b087c24a321b96df919f04484ce5426
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-05-05 05:43:51 +0300

    Revert "Fix"
    This reverts commit b0e161035bd3b8c6c4c1af10e7d0341bebc48203.

--- hwproj.front/src/components/Courses/Courses.tsx
+++ hwproj.front/src/components/Courses/Courses.tsx
@@ -41,7 +41,7 @@ export default class Courses extends React.Component<Props, ICoursesState> {
         const activeCourses = myCourses.filter(course => !course.isCompleted)
         const completedCourses = myCourses.filter(course => course.isCompleted)
         const isExpert = this.props.isExpert
-        let activeCoursesTab = !isLoaded || activeCourses.length > 0 ? 0 : undefined
+        let activeCoursesTab = activeCourses.length > 0 ? 0 : undefined
         let allCoursesTab = activeCoursesTab === 0 ? 1 : 0
         let completedCoursesTab = completedCourses.length > 0
             ? activeCoursesTab === 0 ? 2 : 1
