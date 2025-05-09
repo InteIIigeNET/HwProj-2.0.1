5fcbf8f468ffd4fe18faf0e81628d943ddf04f07
yuri_ufimtsev <ufimtsev3007@gmail.com>
2025-05-09 21:14:23 +0300

    add MappingExtensions with Scope<->ScopeDTO mapping (enum <-> string)

---
+++ HwProj.ContentService/HwProj.ContentService.API/Extensions/MappingExtensions.cs
@@ -0,0 +1,22 @@
+using HwProj.ContentService.API.Models;
+using HwProj.Models.ContentService.DTO;
+
+namespace HwProj.ContentService.API.Extensions;
+
+public static class MappingExtensions
+{
+    public static Scope ToScope(this ScopeDTO scopeDTO)
+        => new Scope(
+            CourseId: scopeDTO.CourseId,
+            CourseUnitType: Enum.Parse<CourseUnitType>(scopeDTO.CourseUnitType),
+            CourseUnitId: scopeDTO.CourseUnitId
+        );
+
+    public static ScopeDTO ToScopeDTO(this Scope scope)
+        => new ScopeDTO
+        {
+            CourseId = scope.CourseId,
+            CourseUnitType = scope.CourseUnitType.ToString(),
+            CourseUnitId = scope.CourseUnitId
+        };
+}
