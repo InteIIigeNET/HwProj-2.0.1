53bdcc4c0d490088544d415ff46d32613f8934d2
yuri_ufimtsev <ufimtsev3007@gmail.com>
2025-05-09 21:12:41 +0300

    refactor S3FilesService (ex. FilesService) and FileKeyService; remove useless FileKeyController

---
+++ HwProj.ContentService/HwProj.ContentService.API/Services/Interfaces/IFileKeyService.cs
@@ -0,0 +1,8 @@
+using HwProj.ContentService.API.Models;
+
+namespace HwProj.ContentService.API.Services.Interfaces;
+
+public interface IFileKeyService
+{
+    public string BuildFileKey(Scope scope, string fileName);
+}
