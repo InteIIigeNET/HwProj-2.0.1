44d1975034024fe56973f0093cc226341950928e
yuri_ufimtsev <ufimtsev3007@gmail.com>
2025-05-09 23:19:59 +0300

    refactor: change FileSize field type in FileRecord entity and DTOs from decimal to long (and measure from KB to bytes)

--- HwProj.Common/HwProj.Models/ContentService/DTO/FileInfoDTO.cs
+++ HwProj.Common/HwProj.Models/ContentService/DTO/FileInfoDTO.cs
@@ -5,7 +5,7 @@ namespace HwProj.Models.ContentService.DTO
         public long Id { get; set; }
         public string Name { get; set; }
         public string Status { get; set; }
-        public decimal SizeInKB { get; set; }
+        public long SizeInBytes { get; set; }
         
         public string CourseUnitType { get; set; }
         public long CourseUnitId { get; set; }
