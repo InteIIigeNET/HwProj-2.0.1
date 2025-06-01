37990ce4fa6251e6f220e6d21edddadf11579c23
yuri_ufimtsev <ufimtsev3007@gmail.com>
2025-06-01 23:23:40 +0300

    refactor: improve ErrorsHandler and FileStatus backend enum

--- HwProj.ContentService/HwProj.ContentService.API/Models/Enums/FileStatus.cs
+++ HwProj.ContentService/HwProj.ContentService.API/Models/Enums/FileStatus.cs
@@ -2,9 +2,9 @@ namespace HwProj.ContentService.API.Models.Enums;
 
 public enum FileStatus
 {
-    Uploading,
-    UploadingError,
-    ReadyToUse,
-    Deleting,
-    DeletingError
+    Uploading = 0,
+    UploadingError = 1,
+    ReadyToUse = 2,
+    Deleting = 3,
+    DeletingError = 4
 }
