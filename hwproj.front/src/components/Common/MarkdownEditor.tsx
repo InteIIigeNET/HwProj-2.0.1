782d623fbe16d583a962bc09ee676902ac852b83
Yuri Ufimtsev <91129832+YuriUfimtsev@users.noreply.github.com>
2025-08-20 00:54:47 +0300

    Поддержать формулы TeX (KaTeX) в редакторе сервиса (#613)

--- hwproj.front/src/components/Common/MarkdownEditor.tsx
+++ hwproj.front/src/components/Common/MarkdownEditor.tsx
@@ -1,4 +1,4 @@
-﻿import {ChangeEvent, FC} from "react";
+﻿import {FC} from "react";
 import MDEditor from "@uiw/react-md-editor";
 import {getCommands, getExtraCommands} from "./Styles/MarkdownEditorCommands.ru";
 import rehypeSanitize, {defaultSchema} from 'rehype-sanitize';
@@ -8,6 +8,8 @@ import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import "./Styles/MarkdownEditor.css";
 import {Schema} from "rehype-sanitize/lib";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 interface MarkdownEditorProps {
     label: string;
@@ -25,12 +27,27 @@ interface MarkdownPreviewProps {
 
 const customRehypeSanitizeSchema: Schema = {
     ...defaultSchema,
-    tagNames: [...defaultSchema.tagNames!, 'code', 'span'],
+    tagNames: [
+        ...(defaultSchema.tagNames || []),
+        // Базовые HTML-теги (разметка и подсветка кода)
+        'span', 'code',
+        // MathML Core:
+        'math', 'mrow', 'mi', 'mo', 'mn', 'msub', 'msup', 'mfrac', 'msqrt',
+        // MathML Advanced:
+        'maction', 'maligngroup', 'malignmark', 'menclose', 'merror',
+        'mfenced', 'mi', 'mlongdiv', 'mmultiscripts', 'mover',
+        'mpadded', 'mphantom', 'mroot', 'ms', 'mscarries', 'mscarry',
+        'msgroup', 'msline', 'mspace', 'msrow', 'mstack', 'mstyle',
+        'msubsup', 'mtable', 'mtd', 'mtext', 'mtr', 'munder', 'munderover',
+        // Аннотации:
+        'semantics', 'annotation', 'annotation-xml'
+    ],
     attributes: {
         ...defaultSchema.attributes,
-        code: ['className'],
-        span: ['className'],
-    },
+        '*': ['className'], // Стилизация KaTeX и подсветка синтаксиса
+        math: ['xmlns'], // Пространство имен MathML
+        annotation: ['encoding'], // Формат аннотации
+    }
 };
 
 const MarkdownPreview: FC<MarkdownPreviewProps> = (props) => <MDEditor.Markdown
@@ -44,24 +61,20 @@ const MarkdownPreview: FC<MarkdownPreviewProps> = (props) => <MDEditor.Markdown
     wrapperElement={{
         "data-color-mode": "light"
     }}
-    rehypePlugins={[[rehypeSanitize, customRehypeSanitizeSchema]]}
+    remarkPlugins={[remarkMath]}
+    rehypePlugins={[
+        [rehypeKatex as any, { output: 'mathml' }],
+        [rehypeSanitize, customRehypeSanitizeSchema]
+    ]}
 />
 
 const MarkdownEditor: FC<MarkdownEditorProps> = (props) => {
-    const handleChange = (value: string | undefined, event?: ChangeEvent<HTMLTextAreaElement>) => {
-        if (value !== undefined) {
-            props.onChange(value!);
-        }
+    const handleChange = (value: string | undefined) => {
+        if (value !== undefined) props.onChange(value);
     };
 
     return (
-        <div
-            data-color-mode="light"
-            style={{
-                marginTop: '15px',
-                marginBottom: '15px',
-            }}
-        >
+        <div data-color-mode="light" style={{marginTop: '15px', marginBottom: '15px'}}>
             <MDEditor
                 commands={[...getCommands()]}
                 extraCommands={[...getExtraCommands()]}
@@ -69,13 +82,15 @@ const MarkdownEditor: FC<MarkdownEditorProps> = (props) => {
                 onChange={handleChange}
                 previewOptions={{
                     className: "markdown-preview",
-                    rehypePlugins: [[rehypeSanitize, customRehypeSanitizeSchema]]
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [
+                        [rehypeKatex as any, { output: 'mathml' }],
+                        [rehypeSanitize, customRehypeSanitizeSchema]
+                    ],
                 }}
                 maxHeight={props.maxHeight ?? 400}
                 height={props.height ?? 230}
-                textareaProps={{
-                    placeholder: props.label
-                }}
+                textareaProps={{placeholder: props.label}}
                 preview="edit"
             />
         </div>
