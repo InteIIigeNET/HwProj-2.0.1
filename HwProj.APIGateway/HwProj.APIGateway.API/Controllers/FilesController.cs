62dbff87e28f8e300575a8c8ff54a00ca3703846
Semyon <semen.rosin1@gmail.com>
2025-10-19 14:38:15 +0300

    feat: change download link validation (back)

--- HwProj.APIGateway/HwProj.APIGateway.API/Controllers/FilesController.cs
+++ HwProj.APIGateway/HwProj.APIGateway.API/Controllers/FilesController.cs
@@ -59,7 +59,7 @@ public class FilesController(
     [ProducesResponseType((int)HttpStatusCode.Forbidden)]
     [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(string[]), (int)HttpStatusCode.NotFound)]
-    public async Task<IActionResult> GetDownloadLink([FromQuery] long fileId)
+    public async Task<IActionResult> GetDownloadLink([FromForm] FileScopeDTO fileScope)
     {
         var linkDto = await contentServiceClient.GetDownloadLinkAsync(fileId);
         if (!linkDto.Succeeded) return BadRequest(linkDto.Errors);
