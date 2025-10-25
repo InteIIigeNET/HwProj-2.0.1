4adb6e03b1ecbf310ea58ebd2b8806bbccaa1c56
Semyon <semen.rosin1@gmail.com>
2025-10-25 17:27:05 +0300

    fix: show solution files uploading status for students only

--- hwproj.front/src/components/Solutions/TaskSolutionComponent.tsx
+++ hwproj.front/src/components/Solutions/TaskSolutionComponent.tsx
@@ -1077,7 +1077,7 @@ const TaskSolutionComponent: FC<ISolutionProps> = (props) => {
                     ) : filesInfo.length > 0 && (
                         <div>
                             <FilesPreviewList
-                                showOkStatus={true}
+                                showOkStatus={ !props.forMentor }
                                 filesInfo={filesInfo}
                                 onClickFileInfo={async (fileInfo: IFileInfo) => {
                                     const url = await ApiSingleton.customFilesApi.getDownloadFileLink(fileInfo.id!, {
