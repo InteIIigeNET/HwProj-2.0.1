8c4e93edfac36ac9eb147850ff0ba21c709e4f7d
Semyon <semen.rosin1@gmail.com>
2025-11-19 13:48:50 +0300

    feat [back]: method to get files scope in info service

--- HwProj.ContentService/HwProj.ContentService.API/Services/Interfaces/IFilesInfoService.cs
+++ HwProj.ContentService/HwProj.ContentService.API/Services/Interfaces/IFilesInfoService.cs
@@ -1,4 +1,5 @@
 using HwProj.ContentService.API.Models;
+using HwProj.ContentService.API.Models.Database;
 using HwProj.ContentService.API.Models.Enums;
 using HwProj.Models.ContentService.DTO;
 
@@ -8,6 +9,7 @@ public interface IFilesInfoService
 {
     public Task<List<FileInfoDTO>> GetFilesStatusesAsync(Scope filesScope);
     public Task<string?> GetFileExternalKeyAsync(long fileId);
+    public Task<Scope?> GetFileScopeAsync(long fileId);
     public Task<List<FileInfoDTO>> GetFilesInfoAsync(long courseId);
     public Task<List<FileInfoDTO>> GetFilesInfoAsync(long courseId, FileStatus filesStatus);
     public Task TransferFilesFromCourse(CourseFilesTransferDto filesTransfer);
