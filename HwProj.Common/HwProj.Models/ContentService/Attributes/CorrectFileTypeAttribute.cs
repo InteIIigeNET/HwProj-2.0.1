1d49d30334db27f459fb84ecb2149817c3d8993b
Semyon <semen.rosin1@gmail.com>
2025-11-20 17:01:23 +0300

    refactor [back]: type validation by foreign library

--- HwProj.Common/HwProj.Models/ContentService/Attributes/CorrectFileTypeAttribute.cs
+++ HwProj.Common/HwProj.Models/ContentService/Attributes/CorrectFileTypeAttribute.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using FileTypeChecker;
 using Microsoft.AspNetCore.Http;
 using FileTypeChecker.Abstracts;
 using FileTypeChecker.Types;
