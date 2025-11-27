c51c79d5ee375c53a50ec76f06ccde87b2bbf9d6
alexmagikov <alexmagikov228@gmail.com>
2025-11-27 15:08:55 +0300

    fix: small fix

--- hwproj.front/vite.config.ts
+++ hwproj.front/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 3000,
-        allowedHosts: ["hwproj.ru", "localhost"],
+        allowedHosts: ["hwproj.ru"],
         hmr: {
             host: 'localhost',
             port: 3000,
