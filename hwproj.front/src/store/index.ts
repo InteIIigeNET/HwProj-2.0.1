e8154586d687c0e58418589674fea507724f7fbb
Andrew Kalinin <Fipett2407@gmail.com>
2026-01-04 14:52:14 +0300

    feat: add courseFileSlice.ts and solutionSlice.ts with update index.ts

--- hwproj.front/src/store/index.ts
+++ hwproj.front/src/store/index.ts
@@ -1,11 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
 import courseReducer from './slices/courseSlice';
 import homeworkReducer from './slices/homeworkSlice';
+import solutionsReducer from './slices/solutionSlice';
+import courseFilesReducer from './slices/courseFileSlice';
 
 export const store = configureStore({
   reducer: {
     course: courseReducer,
     homework: homeworkReducer,
+    solutions: solutionsReducer,
+    courseFiles: courseFilesReducer,
   },
 });
 
