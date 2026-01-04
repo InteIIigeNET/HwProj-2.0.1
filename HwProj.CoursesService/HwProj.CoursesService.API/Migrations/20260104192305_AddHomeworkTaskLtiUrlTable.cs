a74f3f041656fd5d5c37efcfeb04a138b6641afd
KirillBorisovich <kirillbenga@gmail.com>
2026-01-04 22:25:41 +0300

    feat: migrated the database

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Migrations/20260104192305_AddHomeworkTaskLtiUrlTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HwProj.CoursesService.API.Migrations
+{
+    public partial class AddHomeworkTaskLtiUrlTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskLtiUrls",
+                columns: table => new
+                {
+                    TaskId = table.Column<long>(nullable: false),
+                    LtiLaunchUrl = table.Column<string>(nullable: false),
+                    ToolId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskLtiUrls", x => x.TaskId);
+                    table.ForeignKey(
+                        name: "FK_TaskLtiUrls_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskLtiUrls");
+        }
+    }
+}
