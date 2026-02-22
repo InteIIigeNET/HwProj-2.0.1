34879a2ea53b3d034e4cd185f4876179dfa5c2cd
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2026-02-22 03:26:01 +0300

    .

--- hwproj.front/src/components/Courses/StudentStats.tsx
+++ hwproj.front/src/components/Courses/StudentStats.tsx
@@ -108,7 +108,7 @@ const StudentStats: React.FC<IStudentStatsProps> = (props) => {
         .filter(h => !h.tags!.includes(BonusTag))
         .flatMap(homework => homework.tasks)
         .reduce((sum, task) => {
-            return sum + (task!.maxRating || 0);
+            return sum + (task!.tags!.includes(BonusTag) ? 0 : (task!.maxRating || 0));
         }, 0)
 
     const testGroups = Lodash(homeworks.filter(h => h.tags!.includes(TestTag)))
@@ -122,7 +122,8 @@ const StudentStats: React.FC<IStudentStatsProps> = (props) => {
     const testsMaxSum = testGroups
         .map(h => h[0])
         .flatMap(homework => homework.tasks)
-        .reduce((sum, task) => sum + (task!.tags!.includes(BonusTag) ? 0 : (task!.maxRating || 0)), 0)
+        .reduce((sum, task) => 
+            sum + (task!.tags!.includes(BonusTag) ? 0 : (task!.maxRating || 0)), 0)
 
     const hasHomeworks = homeworksMaxSum > 0
     const hasTests = testsMaxSum > 0
