21fc69ea0b3d21a800954faf421a92e829b18e7a
Росин Семён <semen.rosin1@gmail.com>
2026-03-05 20:46:48 +0300

    fix: bug of accounting deleted files in files limiter

--- HwProj.APIGateway/HwProj.APIGateway.API/Filters/FilesCountLimiter.cs
+++ HwProj.APIGateway/HwProj.APIGateway.API/Filters/FilesCountLimiter.cs
@@ -17,7 +17,7 @@ public class FilesCountLimiter(IContentServiceClient contentServiceClient)
         var existingStatuses = await contentServiceClient.GetFilesStatuses(processFilesDto.FilesScope);
         if (!existingStatuses.Succeeded) return false;
 
-        var existingIds = existingStatuses.Value.Select(f => f.Id).ToList();
+        var existingIds = existingStatuses.Value.Where(f => f.Status == "ReadyToUse").Select(f => f.Id).ToList();
         if (processFilesDto.DeletingFileIds.Any(id => !existingIds.Contains(id)))
             return false;
 
