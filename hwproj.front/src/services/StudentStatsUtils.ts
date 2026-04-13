2b1fcd501d18f2712d9e88d954bc621e6445edbc
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2026-04-14 01:36:42 +0300

    wip

--- hwproj.front/src/services/StudentStatsUtils.ts
+++ hwproj.front/src/services/StudentStatsUtils.ts
@@ -1,6 +1,7 @@
 ﻿import {SolutionDto, SolutionState} from "@/api";
 import {colorBetween} from "./JsUtils";
 import Utils from "./Utils";
+import {grey} from "@material-ui/core/colors";
 
 export default class StudentStatsUtils {
 
@@ -17,7 +18,7 @@ export default class StudentStatsUtils {
             : "#ffffff"
     }
 
-    static calculateLastRatedSolution(solutions: SolutionDto[]){
+    static calculateLastRatedSolution(solutions: SolutionDto[]) {
         const ratedSolutions = solutions!.filter(x => x.state !== SolutionState.NUMBER_0)
         return ratedSolutions.slice(-1)[0]
     }
@@ -30,7 +31,7 @@ export default class StudentStatsUtils {
         const lastRatedSolution = ratedSolutions.slice(-1)[0]
 
         let solutionsDescription: string
-        if(disabled)
+        if (disabled)
             solutionsDescription = "Задача недоступна для этого студента"
         else if (lastSolution === undefined)
             solutionsDescription = "Решение отсутствует"
@@ -41,8 +42,8 @@ export default class StudentStatsUtils {
         else solutionsDescription = "Последняя оценка — " + `${lastRatedSolution.rating}/${taskMaxRating} ${Utils.pluralizeHelper(["балл", "балла", "баллов"], taskMaxRating)}\nНовое решение ожидает проверки`
 
         let color: string
-        if(disabled)
-            color = "#eaeaea"
+        if (disabled)
+            color = grey[300]
         else if (lastRatedSolution == undefined)
             color = "#ffffff"
         else
