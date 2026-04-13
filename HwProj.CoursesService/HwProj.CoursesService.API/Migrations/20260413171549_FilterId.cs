90d370a3b1307029781992342dee54eb48a7ee4b
Росин Семён <semen.rosin1@gmail.com>
2026-04-13 20:19:50 +0300

    add migration

---
+++ HwProj.CoursesService/HwProj.CoursesService.API/Migrations/20260413171549_FilterId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HwProj.CoursesService.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FilterId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "UserToCourseFilters",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "UserToCourseFilters",
+                newName: "UserId");
+        }
+    }
+}
