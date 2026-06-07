ffa9e9a189970359172b5d6f430803babc383815
Senator333 <nkir9222@gmail.com>
2026-06-07 15:30:30 +0300

    Update App.tsx
    Remove unnecessary formatting

--- hwproj.front/src/App.tsx
+++ hwproj.front/src/App.tsx
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import React, {Component} from "react";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import "./App.css";
 import "./components/Courses/Course";
 import Course from "./components/Courses/Course";
 import Courses from "./components/Courses/Courses";
 import StudentStatsChart from "./components/Courses/Statistics/StudentStatsChart";
-import { CreateCourse } from "./components/Courses/CreateCourse";
+import {CreateCourse} from "./components/Courses/CreateCourse";
 import Notifications from "./components/Notifications";
 import Workspace from "./components/Workspace";
 import TaskSolutionsPage from "./components/Solutions/TaskSolutionsPage";
-import {
-  AppBarContextAction,
-  appBarStateManager,
-  Header,
-} from "./components/AppBar";
+import {AppBarContextAction, appBarStateManager, Header} from "./components/AppBar";
 import Login from "./components/Auth/Login";
 import EditCourse from "./components/Courses/EditCourse";
 import Register from "./components/Auth/Register";
@@ -33,141 +29,112 @@ import PromoPage from "./pages/PromoPage";
 // TODO: add flux
 
 interface AppState {
-  loggedIn: boolean;
-  isLecturer: boolean;
-  isExpert: boolean;
-  newNotificationsCount: number;
-  appBarContextAction: AppBarContextAction;
+    loggedIn: boolean;
+    isLecturer: boolean;
+    isExpert: boolean;
+    newNotificationsCount: number;
+    appBarContextAction: AppBarContextAction
 }
 
 const withRouter = (Component: any) => {
-  return (props: any) => {
-    const navigate = useNavigate();
+    return (props: any) => {
+        const navigate = useNavigate();
 
-    return <Component navigate={navigate} {...props} />;
-  };
+        return (
+            <Component
+                navigate={navigate}
+                {...props}
+            />
+        );
+    };
 };
 
 class App extends Component<{ navigate: any }, AppState> {
-  constructor(props: { navigate: any }) {
-    super(props);
-    this.state = {
-      loggedIn: ApiSingleton.authService.isLoggedIn(),
-      isLecturer: ApiSingleton.authService.isLecturer(),
-      isExpert: ApiSingleton.authService.isExpert(),
-      newNotificationsCount: 0,
-      appBarContextAction: "Default",
-    };
-    appBarStateManager.setOnContextActionChange((appBarState) =>
-      this.setState({ appBarContextAction: appBarState }),
-    );
-  }
-
-  componentDidMount = async () => {
-    await this.updatedNewNotificationsCount();
-  };
-
-  updatedNewNotificationsCount = async () => {
-    if (ApiSingleton.authService.isLoggedIn()) {
-      const data = await ApiSingleton.notificationsApi.notificationsGetNewNotificationsCount();
-      this.setState({ newNotificationsCount: data });
+    constructor(props: { navigate: any }) {
+        super(props);
+        this.state = {
+            loggedIn: ApiSingleton.authService.isLoggedIn(),
+            isLecturer: ApiSingleton.authService.isLecturer(),
+            isExpert: ApiSingleton.authService.isExpert(),
+            newNotificationsCount: 0,
+            appBarContextAction: "Default"
+        };
+        appBarStateManager.setOnContextActionChange(appBarState => this.setState({appBarContextAction: appBarState}))
     }
-  };
 
-  login = async (returnUrl: string | null) => {
-    const isLecturer = ApiSingleton.authService.isLecturer();
-    const isExpert = ApiSingleton.authService.isExpert();
-    this.setState({
-      loggedIn: true,
-      isLecturer: isLecturer,
-      isExpert: isExpert,
-    });
-    if (!isExpert) {
-      await this.updatedNewNotificationsCount();
-      this.props.navigate(returnUrl || "/");
+    componentDidMount = async () => {
+        await this.updatedNewNotificationsCount()
     }
-  };
 
-  logout = () => {
-    ApiSingleton.authService.logout();
-    this.setState({
-      loggedIn: false,
-      isLecturer: false,
-      isExpert: false,
-      newNotificationsCount: 0,
-    });
-    this.props.navigate("/login");
-  };
+    updatedNewNotificationsCount = async () => {
+        if (ApiSingleton.authService.isLoggedIn()) {
+            const data = await ApiSingleton.notificationsApi.notificationsGetNewNotificationsCount()
+            this.setState({newNotificationsCount: data})
+        }
+    }
 
-  render() {
-    return (
-      <>
-        <Header
-          loggedIn={this.state.loggedIn}
-          newNotificationsCount={this.state.newNotificationsCount}
-          isLecturer={this.state.isLecturer}
-          isExpert={this.state.isExpert}
-          onLogout={this.logout}
-          contextAction={this.state.appBarContextAction}
-        />
-        <TrackPageChanges />
-        <Routes>
-          <Route element={<AuthLayout />}>
-            <Route
-              path="user/edit"
-              element={<EditProfile isExpert={this.state.isExpert} />}
-            />
-            <Route path="/" element={<Workspace />} />
-            <Route
-              path="notifications"
-              element={
-                <Notifications
-                  onMarkAsSeen={this.updatedNewNotificationsCount}
-                />
-              }
-            />
-            <Route
-              path="courses"
-              element={
-                <Courses
-                  navigate={this.props.navigate}
-                  isLecturer={this.state.isLecturer}
-                  isExpert={this.state.isExpert}
-                />
-              }
-            />
-            <Route path="profile/:id" element={<Workspace />} />
-            <Route path="experts" element={<ExpertsNotebook />} />
-            <Route path="create_course" element={<CreateCourse />} />
-            <Route path="courses/:courseId" element={<Course />} />
-            <Route path="courses/:courseId/:tab" element={<Course />} />
-            <Route path="courses/:courseId/editInfo" element={<EditCourse />} />
-            <Route
-              path="task/:taskId/:studentId"
-              element={<StudentSolutionsPage />}
-            />
-            <Route path="task/:taskId/" element={<TaskSolutionsPage />} />
-          </Route>
-          <Route
-            path="statistics/:courseId/charts"
-            element={<StudentStatsChart />}
-          />
-          <Route path="status" element={<SystemInfoComponent />} />
-          <Route path="login" element={<Login onLogin={this.login} />} />
-          <Route path="register" element={<Register />} />
-          <Route path="recovery" element={<PasswordRecovery />} />
-          <Route path="resetPassword" element={<ResetPassword />} />
-          <Route
-            path="join/:token"
-            element={<ExpertAuthLayout onLogin={this.login} />}
-          />
-          <Route path="/promo" element={<PromoPage />} />
-          <Route path={"*"} element={<WrongPath />} />
-        </Routes>
-        <div style={{ marginBottom: 10 }} />
-      </>
-    );
-  }
+    login = async (returnUrl: string | null) => {
+        const isLecturer = ApiSingleton.authService.isLecturer();
+        const isExpert = ApiSingleton.authService.isExpert();
+        this.setState({
+            loggedIn: true,
+            isLecturer: isLecturer,
+            isExpert: isExpert
+        })
+        if (!isExpert) {
+            await this.updatedNewNotificationsCount()
+            this.props.navigate(returnUrl || "/");
+        }
+    }
+
+    logout = () => {
+        ApiSingleton.authService.logout();
+        this.setState({loggedIn: false, isLecturer: false, isExpert: false, newNotificationsCount: 0});
+        this.props.navigate("/login");
+    }
+
+    render() {
+        return (
+            <>
+                <Header loggedIn={this.state.loggedIn}
+                        newNotificationsCount={this.state.newNotificationsCount}
+                        isLecturer={this.state.isLecturer}
+                        isExpert={this.state.isExpert}
+                        onLogout={this.logout}
+                        contextAction={this.state.appBarContextAction}/>
+                <TrackPageChanges/>
+                <Routes>
+                    <Route element={<AuthLayout/>}>
+                        <Route path="user/edit" element={<EditProfile isExpert={this.state.isExpert}/>}/>
+                        <Route path="/" element={<Workspace/>}/>
+                        <Route path="notifications"
+                               element={<Notifications onMarkAsSeen={this.updatedNewNotificationsCount}/>}/>
+                        <Route path="courses"
+                               element={<Courses navigate={this.props.navigate} isLecturer={this.state.isLecturer}
+                                                 isExpert={this.state.isExpert}/>}/>
+                        <Route path="profile/:id" element={<Workspace/>}/>
+                        <Route path="experts" element={<ExpertsNotebook/>}/>
+                        <Route path="create_course" element={<CreateCourse/>}/>
+                        <Route path="courses/:courseId" element={<Course/>}/>
+                        <Route path="courses/:courseId/:tab" element={<Course/>}/>
+                        <Route path="courses/:courseId/editInfo" element={<EditCourse/>}/>
+                        <Route path="task/:taskId/:studentId" element={<StudentSolutionsPage/>}/>
+                        <Route path="task/:taskId/" element={<TaskSolutionsPage/>}/>
+                    </Route>
+                    <Route path="statistics/:courseId/charts" element={<StudentStatsChart/>}/>
+                    <Route path="status" element={<SystemInfoComponent/>}/>
+                    <Route path="login" element={<Login onLogin={this.login}/>}/>
+                    <Route path="register" element={<Register/>}/>
+                    <Route path="recovery" element={<PasswordRecovery/>}/>
+                    <Route path="resetPassword" element={<ResetPassword/>}/>
+                    <Route path="join/:token" element={<ExpertAuthLayout onLogin={this.login}/>}/>
+                    <Route path="/promo" element={<PromoPage/>}/>
+                    <Route path={"*"} element={<WrongPath/>}/>
+                </Routes>
+                <div style={{marginBottom: 10}}/>
+            </>
+        );
+    }
 }
 
 export default withRouter(App);
